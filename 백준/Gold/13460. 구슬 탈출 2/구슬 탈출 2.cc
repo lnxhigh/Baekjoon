6bbf4c59034a2f20bcaaef0510619d69e4d34a66
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2024-01-14 09:36:15 +0900

    [Gold I] Title: 구슬 탈출 2, Time: 56 ms, Memory: 23712 KB -BaekjoonHub

--- 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.cc
+++ 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.cc
@@ -56,388 +56,52 @@ void print(Marble marble, char board[11][11]) {
     board[blue.first][blue.second] = '.';
 }
 
-
-Marble up_(Marble marble, char board[11][11]) {
-    xy red = marble.red;
-    xy blue = marble.blue;
-
-    if (red.second == blue.second) {
-        if (red.first < blue.first) {
-            // move red first
-            board[blue.first][blue.second] = 'B';
-            board[destination.first][destination.second] = 'O';
-            red.first--;
-            while (true) {
-                char x = board[red.first][red.second];
-                if (x == '.') { red.first--; }
-                else if (x == 'O') { break; }
-                else if (x == '#') { red.first++; break; }
-                else if (x == 'B') { red.first++; break; }
-                else { cout << "Error"; }
-            }
-            board[blue.first][blue.second] = '.';
-
-            // then blue next
-            board[red.first][red.second] = 'R';
-            board[destination.first][destination.second] = 'O';
-            blue.first--;
-            while (true) {
-                char x = board[blue.first][blue.second];
-                if (x == '.') { blue.first--; }
-                else if (x == 'O') { break; }
-                else if (x == '#') { blue.first++; break; }
-                else if (x == 'R') { blue.first++; break; }
-                else { cout << "Error"; }
-            }
-            board[red.first][red.second] = '.';
-        }
-        
-        else if (red.first > blue.first) {
-            // move blue first
-            board[red.first][red.second] = 'R';
-            board[destination.first][destination.second] = 'O';
-            blue.first--;
-            while (true) {
-                char x = board[blue.first][blue.second];
-                if (x == '.') { blue.first--; }
-                else if (x == 'O') { break; }
-                else if (x == '#') { blue.first++; break; }
-                else if (x == 'R') { blue.first++; break; }
-                else { cout << "Error"; }
-            }
-            board[red.first][red.second] = '.';
-            
-            // then red next
-            board[blue.first][blue.second] = 'B';
-            board[destination.first][destination.second] = 'O';
-            red.first--;
-            while (true) {
-                char x = board[red.first][red.second];
-                if (x == '.') { red.first--; }
-                else if (x == '#') { red.first++; break; }
-                else if (x == 'B') { red.first++; break; }
-                else if (x == 'O') { break; }
-                else { cout << "Error"; }
-            }
-            board[blue.first][blue.second] = '.';
-        }
-    }
-    else if (red.second != blue.second) {
-        // move red and blue seperately
-        
-        // move red
-        red.first--;
-        while (true) {
-            char x = board[red.first][red.second];
-            if (x == '.') { red.first--; }
-            else if (x == 'O') { break; }
-            else if (x == '#') { red.first++; break; }
-            else { cout << "Error"; }
-        }
-        
-        // move blue
-        blue.first--;
-        while (true) {
-            char x = board[blue.first][blue.second];
-            if (x == '.') { blue.first--; }
-            else if (x == 'O') { break; }
-            else if (x == '#') { blue.first++; break; }
-            else { cout << "Error"; }
-        }
-    }
-
-    return { red, blue };
-}
-
-
-Marble down_(Marble marble, char board[11][11]) {
-    xy red = marble.red;
-    xy blue = marble.blue;
-
-    if (red.second == blue.second) {
-        if (red.first > blue.first) {
-            // move red first
-            board[blue.first][blue.second] = 'B';
-            board[destination.first][destination.second] = 'O';
-            red.first++;
-            while (true) {
-                char x = board[red.first][red.second];
-                if (x == '.') { red.first++; }
-                else if (x == 'O') { break; }
-                else if (x == '#') { red.first--; break; }
-                else if (x == 'B') { red.first--; break; }
-                else { cout << "Error"; }
-            }
-            board[blue.first][blue.second] = '.';
-
-            // then blue next
-            board[red.first][red.second] = 'R';
-            board[destination.first][destination.second] = 'O';
-            blue.first++;
-            while (true) {
-                char x = board[blue.first][blue.second];
-                if (x == '.') { blue.first++; }
-                else if (x == 'O') { break; }
-                else if (x == '#') { blue.first--; break; }
-                else if (x == 'R') { blue.first--; break; }
-                else { cout << "Error"; }
-            }
-            board[red.first][red.second] = '.';
-        }
-        
-        else if (red.first < blue.first) {
-            // move blue first
-            board[red.first][red.second] = 'R';
-            board[destination.first][destination.second] = 'O';
-            blue.first++;
-            while (true) {
-                char x = board[blue.first][blue.second];
-                if (x == '.') { blue.first++; }
-                else if (x == 'O') { break; }
-                else if (x == '#') { blue.first--; break; }
-                else if (x == 'R') { blue.first--; break; }
-                else { cout << "Error"; }
-            }
-            board[red.first][red.second] = '.';
-            
-            // then red next
-            board[blue.first][blue.second] = 'B';
-            board[destination.first][destination.second] = 'O';
-            red.first++;
-            while (true) {
-                char x = board[red.first][red.second];
-                if (x == '.') { red.first++; }
-                else if (x == '#') { red.first--; break; }
-                else if (x == 'B') { red.first--; break; }
-                else if (x == 'O') { break; }
-                else { cout << "Error"; }
-            }
-            board[blue.first][blue.second] = '.';
-        }
-    }
-    else if (red.second != blue.second) {
-        // move red and blue seperately
-        
-        // move red
-        red.first++;
-        while (true) {
-            char x = board[red.first][red.second];
-            if (x == '.') { red.first++; }
-            else if (x == 'O') { break; }
-            else if (x == '#') { red.first--; break; }
-            else { cout << "Error"; }
-        }
-        
-        // move blue
-        blue.first++;
-        while (true) {
-            char x = board[blue.first][blue.second];
-            if (x == '.') { blue.first++; }
-            else if (x == 'O') { break; }
-            else if (x == '#') { blue.first--; break; }
-            else { cout << "Error"; }
-        }
-    }
-
-    return { red, blue };
-}
-
-
-Marble left_(Marble marble, char board[11][11]) {
-    xy red = marble.red;
-    xy blue = marble.blue;
-
-    if (red.first == blue.first) {
-        if (red.second < blue.second) {
-            // move red first
-            board[blue.first][blue.second] = 'B';
-            board[destination.first][destination.second] = 'O';
-            red.second--;
-            while (true) {
-                char x = board[red.first][red.second];
-                if (x == '.') { red.second--; }
-                else if (x == 'O') { break; }
-                else if (x == '#') { red.second++; break; }
-                else if (x == 'B') { red.second++; break; }
-                else { cout << "Error"; }
-            }
-            board[blue.first][blue.second] = '.';
-
-            // then blue next
-            board[red.first][red.second] = 'R';
-            board[destination.first][destination.second] = 'O';
-            blue.second--;
-            while (true) {
-                char x = board[blue.first][blue.second];
-                if (x == '.') { blue.second--; }
-                else if (x == 'O') { break; }
-                else if (x == '#') { blue.second++; break; }
-                else if (x == 'R') { blue.second++; break; }
-                else { cout << "Error"; }
-            }
-            board[red.first][red.second] = '.';
-        }
-        
-        else if (red.second > blue.second) {
-            // move blue first
-            board[red.first][red.second] = 'R';
-            board[destination.first][destination.second] = 'O';
-            blue.second--;
-            while (true) {
-                char x = board[blue.first][blue.second];
-                if (x == '.') { blue.second--; }
-                else if (x == 'O') { break; }
-                else if (x == '#') { blue.second++; break; }
-                else if (x == 'R') { blue.second++; break; }
-                else { cout << "Error"; }
-            }
-            board[red.first][red.second] = '.';
-            
-            // then red next
-            board[blue.first][blue.second] = 'B';
-            board[destination.first][destination.second] = 'O';
-            red.second--;
-            while (true) {
-                char x = board[red.first][red.second];
-                if (x == '.') { red.second--; }
-                else if (x == '#') { red.second++; break; }
-                else if (x == 'B') { red.second++; break; }
-                else if (x == 'O') { break; }
-                else { cout << "Error"; }
-            }
-            board[blue.first][blue.second] = '.';
-        }
-    }
-    else if (red.first != blue.first) {
-        // move red and blue seperately
-        
-        // move red
-        red.second--;
-        while (true) {
-            char x = board[red.first][red.second];
-            if (x == '.') { red.second--; }
-            else if (x == 'O') { break; }
-            else if (x == '#') { red.second++; break; }
-            else { cout << "Error"; }
-        }
-        
-        // move blue
-        blue.second--;
-        while (true) {
-            char x = board[blue.first][blue.second];
-            if (x == '.') { blue.second--; }
-            else if (x == 'O') { break; }
-            else if (x == '#') { blue.second++; break; }
-            else { cout << "Error"; }
-        }
-    }
-
-    return { red, blue };
-}
-
-
-Marble right_(Marble marble, char board[11][11]) {
-    xy red = marble.red;
-    xy blue = marble.blue;
-
-    if (red.first == blue.first) {
-        if (red.second > blue.second) {
-            // move red first
-            board[blue.first][blue.second] = 'B';
-            board[destination.first][destination.second] = 'O';
-            red.second++;
-            while (true) {
-                char x = board[red.first][red.second];
-                if (x == '.') { red.second++; }
-                else if (x == 'O') { break; }
-                else if (x == '#') { red.second--; break; }
-                else if (x == 'B') { red.second--; break; }
-                else { cout << "Error"; }
-            }
-            board[blue.first][blue.second] = '.';
-
-            // then blue next
-            board[red.first][red.second] = 'R';
-            board[destination.first][destination.second] = 'O';
-            blue.second++;
-            while (true) {
-                char x = board[blue.first][blue.second];
-                if (x == '.') { blue.second++; }
-                else if (x == 'O') { break; }
-                else if (x == '#') { blue.second--; break; }
-                else if (x == 'R') { blue.second--; break; }
-                else { cout << "Error"; }
-            }
-            board[red.first][red.second] = '.';
-        }
-        
-        else if (red.second < blue.second) {
-            // move blue first
-            board[red.first][red.second] = 'R';
-            board[destination.first][destination.second] = 'O';
-            blue.second++;
-            while (true) {
-                char x = board[blue.first][blue.second];
-                if (x == '.') { blue.second++; }
-                else if (x == 'O') { break; }
-                else if (x == '#') { blue.second--; break; }
-                else if (x == 'R') { blue.second--; break; }
-                else { cout << "Error"; }
-            }
-            board[red.first][red.second] = '.';
-            
-            // then red next
-            board[blue.first][blue.second] = 'B';
-            board[destination.first][destination.second] = 'O';
-            red.second++;
-            while (true) {
-                char x = board[red.first][red.second];
-                if (x == '.') { red.second++; }
-                else if (x == '#') { red.second--; break; }
-                else if (x == 'B') { red.second--; break; }
-                else if (x == 'O') { break; }
-                else { cout << "Error"; }
-            }
-            board[blue.first][blue.second] = '.';
-        }
-    }
-    else if (red.first != blue.first) {
-        // move red and blue seperately
-        
-        // move red
-        red.second++;
-        while (true) {
-            char x = board[red.first][red.second];
-            if (x == '.') { red.second++; }
-            else if (x == 'O') { break; }
-            else if (x == '#') { red.second--; break; }
-            else { cout << "Error"; }
-        }
-        
-        // move blue
-        blue.second++;
-        while (true) {
-            char x = board[blue.first][blue.second];
-            if (x == '.') { blue.second++; }
-            else if (x == 'O') { break; }
-            else if (x == '#') { blue.second--; break; }
-            else { cout << "Error"; }
-        }
-    }
-
-    return { red, blue };
-}
-
-
 Marble moveTo(Marble marble, int i, char board[11][11]) {
-    if      (i == 1) return up_(marble, board);
-    else if (i == 2) return down_(marble, board);
-    else if (i == 3) return left_(marble, board);
-    else if (i == 4) return right_(marble, board);
-    else {
-        cout << "Error Move" << '\n';
-        return marble;
+    xy red = marble.red;
+    xy blue = marble.blue;
+    int n_red_moved = 0, n_blue_moved = 0;
+
+    // Up Down Left Right
+    int dx[4] = {-1,+1, 0, 0};
+    int dy[4] = { 0, 0,-1,+1};
+
+    while (board[red.first][red.second] == '.') {
+        red.first += dx[i];
+        red.second += dy[i];
+        n_red_moved++;
     }
+    
+    while (board[blue.first][blue.second] == '.') {
+        blue.first += dx[i];
+        blue.second += dy[i];
+        n_blue_moved++;
+    }
+
+    if (board[red.first][red.second] == '#') {
+        red.first -= dx[i]; 
+        red.second -= dy[i];
+    }
+    if (board[blue.first][blue.second] == '#') {
+        blue.first -= dx[i];
+        blue.second -= dy[i];
+    }
+
+    if (red == destination and blue == destination) {
+        return { red, blue };
+    }
+
+    if (red == blue) {
+        if (n_red_moved > n_blue_moved) {
+            red.first -= dx[i];
+            red.second -= dy[i];
+        }
+        else {
+            blue.first -= dx[i];
+            blue.second -= dy[i];
+        }
+    }
+    
+    return { red, blue };
 }
 
 int bfs() {
@@ -448,19 +112,13 @@ int bfs() {
         q.pop();
         if (depth > 10) return -1;
 
-        for (int i = 1; i <= 4; i++) {
+        for (int i = 0; i < 4; i++) {
             Marble next = moveTo(cur, i, board);
             if (next.red != destination and next.blue != destination) {
                 q.push({ next, depth+1 });
             }
             else if (next.red == destination and next.blue != destination) {
                 return depth; 
-            }
-            else if (next.red != destination and next.blue == destination) {
-                continue; 
-            }
-            else {
-                continue; 
             }
         }
     }
