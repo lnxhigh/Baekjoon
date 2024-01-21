3882413f4243a1512616d0ac58e715a68048fa19
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2024-01-21 16:44:57 +0900

    [Gold I] Title: 계단 수, Time: 8 ms, Memory: 6060 KB -BaekjoonHub

--- 백준/Gold/1562. 계단 수/계단 수.cc
+++ 백준/Gold/1562. 계단 수/계단 수.cc
@@ -1,10 +1,9 @@
 #include <iostream>
-#define K 1000000000LL
-#define ll long long
+#define K 1000000000
 using namespace std;
 
-ll N;
-ll stairs[101][10][1<<10] = {0LL,}; // len =  N; last digit = i; kth bit = 1 if there is at least one k
+int N;
+int stairs[101][10][1<<10] = {0,}; // len =  N; last digit = i; kth bit = 1 if there is at least one k
 
 int main() {
     ios::sync_with_stdio(false);
@@ -13,14 +12,14 @@ int main() {
     cin >> N;
 
     // N == 1
-    for (ll i = 0; i < 10; i++) {
+    for (int i = 0; i < 10; i++) {
         stairs[1][i][1 << i] = 1;
     }
     
     // N >= 2
-    for (ll n = 1; n <= N-1; n++) {
-        for (ll bit = 0; bit < 1024; bit++) {
-            for (ll i = 0; i <= 9; i++) {
+    for (int n = 1; n <= N-1; n++) {
+        for (int bit = 0; bit < 1024; bit++) {
+            for (int i = 0; i <= 9; i++) {
                 if (i - 1 >= 0) {
                     stairs[n+1][i-1][bit | (1 << (i-1))] = (
                         stairs[n+1][i-1][bit | (1 << (i-1))] + stairs[n][i][bit]
@@ -35,7 +34,7 @@ int main() {
         }
     }
 
-    ll ans = 0;
+    int ans = 0;
     for (int i = 1; i <= 9; i++) {
         ans = ( ans + stairs[N][i][(1 << 10) - 1] ) % K;
     }
