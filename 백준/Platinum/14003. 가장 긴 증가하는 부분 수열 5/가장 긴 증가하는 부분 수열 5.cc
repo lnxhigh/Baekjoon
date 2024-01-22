139aeede364766a105af1b5f21b54683ba8ca6eb
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2024-01-22 13:55:27 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 236 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#define N_MAX 1000000
+using namespace std;
+
+// https://en.wikipedia.org/wiki/Longest_increasing_subsequence
+
+int N;
+int X[N_MAX];
+int P[N_MAX];
+int M[N_MAX+1];
+int S[N_MAX];
+int L = 0;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> X[i];
+    }
+
+    M[0] = -1, M[1] = 0;
+
+    for (int i = 0; i < N; i++) {
+        int low = 1;
+        int high = L + 1;
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (X[M[mid]] >= X[i]) high = mid;
+            else low = mid + 1;
+        }
+        
+        P[i] = M[low-1];
+        M[low] = i;
+
+        if (low > L) {
+            L = low;
+        }
+    }
+
+    int k = M[L];
+    for (int i = L-1; i >= 0; i--) {
+        S[i] = X[k];
+        k = P[k];
+    }
+
+    cout << L << '\n';
+    for (int i = 0; i < L; i++) {
+        cout << S[i] << ' ';
+    }
+    cout << '\n';
+}
