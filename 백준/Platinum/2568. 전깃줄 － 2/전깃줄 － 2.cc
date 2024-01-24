e09ac2aee1d815a64053f891b6babb774b67f2cd
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2024-01-25 07:49:30 +0900

    [Platinum V] Title: 전깃줄 - 2, Time: 96 ms, Memory: 16728 KB -BaekjoonHub

---
+++ 백준/Platinum/2568. 전깃줄 － 2/전깃줄 － 2.cc
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <utility>
+#include <algorithm>
+#include <set>
+#define N_MAX 100001
+#define LOC_MAX 500001
+using namespace std;
+
+int N;
+int A[LOC_MAX] = {0,};
+int B[LOC_MAX] = {0,};
+pair<int,int> AB[N_MAX];
+multiset<int> Aset;
+multiset<int> Bset;
+
+int X[N_MAX];
+int P[N_MAX];
+int M[N_MAX+1];
+int S[N_MAX];
+int L = 0;
+
+// Longest Increasing Subsequence
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        int a, b;
+        cin >> a >> b;
+        A[a] = b, B[b] = a;
+        AB[i] = {a,b};
+        Aset.insert(a); Bset.insert(b);
+    }
+
+    sort(AB, AB + N);
+    for (int i = 0; i < N; i++) {
+        X[i] = AB[i].second;
+    }
+
+    // LIS
+    M[0] = -1, M[1] = 0;
+    
+    for (int i = 0; i < N; i++) {
+        int low = 1;
+        int high = L + 1;
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (X[M[mid]] >= X[i]) high = mid;
+            else low = mid + 1;
+        }
+        
+        P[i] = M[low-1];
+        M[low] = i;
+
+        if (low > L) L = low;
+    }
+
+    int k = M[L];
+    for (int i = L-1; i >= 0; i--) {
+        S[i] = X[k];
+        k = P[k];
+    }
+
+    for (int i = 0; i < L; i++) {
+        Aset.erase(B[S[i]]);
+    }
+    
+    cout << N - L << '\n';
+    for (auto iter = Aset.begin(); iter != Aset.end(); iter++) {
+        cout << *iter << '\n';
+    }
+
+    return 0;
+}
