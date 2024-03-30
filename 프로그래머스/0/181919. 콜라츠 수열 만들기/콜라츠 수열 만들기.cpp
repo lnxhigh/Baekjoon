974406bb5000f74c61ea3bf270c6d1429548e979
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2024-03-30 07:19:10 +0100

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> solution(int n) {
+    vector<int> answer;
+    while (n != 1) {
+        answer.push_back(n);
+        if (n & 1) n = 3 * n + 1;
+        else n /= 2;
+    }
+    answer.push_back(n);
+    
+    return answer;
+}
