e0ab68e1b2e6969584e4211a93d87f13c52718c9
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2024-03-30 05:32:23 +0100

    [level 0] Title: n 번째 원소까지, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list, int n) {
+    vector<int> answer(num_list.begin(), num_list.begin() + n);
+    return answer;
+    
+    
+}
