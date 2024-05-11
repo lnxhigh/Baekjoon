592a5733cd950ae5386a47155c5a76cbf28612b0
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2024-05-11 21:40:56 +0900

    [Gold III] Title: K의 배수, Time: 4 ms, Memory: 2416 KB -BaekjoonHub

--- 백준/Gold/30025. K의 배수/K의 배수.cc
+++ 백준/Gold/30025. K의 배수/K의 배수.cc
@@ -1,46 +1,34 @@
 #include <bits/stdc++.h>
-#define ll long long
-#define MOD 1000000007LL
+#define MOD 1000000007
 using namespace std;
 
-ll N, M, K;
-vector<ll> arr;
+int N, M, K, X;
+vector<int> arr;
+int dp[101][1000] = {0,};
 
-ll dp[101][1000] = {0,};
-
-int main() 
-{
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr); cout.tie(nullptr);
     
     cin >> N >> M >> K;
-    for (ll i = 0; i < N; i++) 
-    {
-        ll x;
-        cin >> x;
-        arr.push_back(x);
+
+    for (int i = 0; i < N; i++) {
+        cin >> X;
+        arr.push_back(X);
+        if (X != 0) dp[1][X % K]++;
     }
 
-    for (ll n : arr)
-    {
-        if (n == 0) continue;
-        dp[1][n % K]++;
-    }
-
-    for (ll i = 1; i < M; i++)
-    {
-        for (ll n : arr)
-        {
-            for (ll k = 0; k < K; k++)
-            {
-                ll remain = (k * (10LL % K) + n) % K;
-                dp[i+1][remain] += dp[i][k];
-                dp[i+1][remain] %= MOD;
+    for (int i = 1; i < M; i++) {
+        for (int n : arr) {
+            for (int k = 0; k < K; k++) {
+                int r = (k * (10 % K) + n) % K;
+                dp[i+1][r] += dp[i][k];
+                dp[i+1][r] %= MOD;
             }
         }
     }
 
-    cout << dp[M][0];
+    cout << dp[M][0] << '\n';
 
     return 0;
 }
