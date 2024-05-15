017fd47584a86f9f5ffb56eba6f222320ae6bd20
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2024-05-16 01:55:44 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N;
+string str;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    
+    while (true) {
+        getline(cin, str);
+        if (str == "") break;
+        cout << str << endl;
+    }
+    
+    return 0;
+}
