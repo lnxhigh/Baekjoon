7742bbcd64c661313b436d7783bcc081e2e53404
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2024-05-26 15:42:04 +0900

    [Silver IV] Title: 상근이의 여행, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int T;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    cin >> T;
+    while (T--) {
+        int N, M;
+        cin >> N >> M;
+        for (int m = 0; m < M; m++) {
+            int a, b;
+            cin >> a >> b;
+        }
+        cout << N - 1 << "\n"; // ...
+    }
+    
+    return 0;
+}
