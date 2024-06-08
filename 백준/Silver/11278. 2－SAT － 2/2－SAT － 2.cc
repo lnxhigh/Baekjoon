478965969cb219990f32a1af7b187e16ce687b43
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2024-06-09 01:27:49 +0900

    [Silver I] Title: 2-SAT - 2, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11278. 2－SAT － 2/2－SAT － 2.cc
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#define M_MAX 100
+using namespace std;
+
+int N, M;
+int clause[M_MAX][2];
+int answer;
+
+bool checkClause(int i, int &state) {
+    for (int k = 0; k < 2; k++) {
+        int x = abs(clause[i][k]) - 1;
+        bool tmp = (clause[i][k] > 0) ^ ((state & (1 << x)) > 0);
+        if (!tmp) return true;
+    }
+    return false;
+}
+
+bool checkAll(int &state) {
+    for (int i = 0; i < M; i++) {
+        if (!checkClause(i, state)) return false;
+    }
+    return true;
+}
+
+bool solve() {
+    for (int i = 0; i < (1 << N); i++) {
+        if (checkAll(i)) {
+            answer = i;
+            return true;
+        }
+    }
+    return false;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> N >> M;
+    for (int i = 0; i < M; i++) {
+        cin >> clause[i][0] >> clause[i][1];
+    }
+
+    bool res = solve();
+    cout << res << '\n';
+    if (res) {
+        for (int i = 0; i < N; i++) {
+            cout << ((answer & (1 << i)) >> i) << ' ';
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
