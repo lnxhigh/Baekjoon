aa1f54509e8bcb983de0dc71b0e266f429e29a1a
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2024-06-19 04:26:04 +0900

    [Gold V] Title: 평범한 배낭, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,26 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N, K;
-const int N_MAX = 101;
-const int K_MAX = 100001;
-
-int W[N_MAX], V[N_MAX];
-int D[K_MAX];
+int N, K, W, V;
+int D[100001];
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
     cin >> N >> K;
-    
-    for (int i = 1; i <= N; i++) {
-        cin >> W[i] >> V[i];
-    }
-
-    // 메모리 절약 => 역순으로 반복하면 되는 것 같다 ...?!
-    for (int i = 1; i <= N; i++) {
-        for (int w = K; w >= W[i]; w--) {
-            D[w] = max(D[w], D[w-W[i]] + V[i]);
+    for (int i = 0; i < N; i++) {
+        cin >> W >> V;
+        for (int w = K; w >= W; w--) {
+            D[w] = max(D[w], D[w-W] + V);
         }
     }
 
