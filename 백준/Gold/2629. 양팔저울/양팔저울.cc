6085c0e1446edafc3a8dd143ca706ecf9b9d428f
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2024-06-20 05:25:47 +0900

    [Gold III] Title: 양팔저울, Time: 0 ms, Memory: 2532 KB -BaekjoonHub

---
+++ 백준/Gold/2629. 양팔저울/양팔저울.cc
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, M, K;
+const int N_MAX = 1 << 5, M_MAX = 1 << 3;
+const int K_MAX = 1 << 14;
+
+int W[N_MAX], X[M_MAX];
+bool D[N_MAX][K_MAX];
+
+int main() {
+    // Input
+    cin >> N;
+    for (int i = 1; i <= N; i++) {
+        cin >> W[i];
+    }
+    cin >> M;
+    for (int i = 1; i <= M; i++) {
+        cin >> X[i];
+    }
+
+    // 최대 무게 설정
+    for (int i = 1; i <= N; i++) {
+        K += W[i];
+    }
+
+    // DP 배열 채우기
+    for (int i = 0; i <= N; i++) {
+        D[i][0] = true;
+    }
+
+    for (int i = 1; i <= N; i++) {
+        for (int k = 1; k <= K; k++) {
+            D[i][k] = D[i-1][k] || D[i-1][abs(W[i]-k)] || D[i-1][k+W[i]];
+        }
+    }
+    
+    // 결과 출력
+    for (int i = 1; i <= M; i++) {
+        char ans;
+        if (X[i] > K) ans = 'N';
+        else if (D[N][X[i]]) ans = 'Y';
+        else ans = 'N';
+        cout << ans << ' ';
+    }
+    cout << '\n';
+
+    return 0;
+}
