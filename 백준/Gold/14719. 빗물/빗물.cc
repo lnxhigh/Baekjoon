355d440e7985afc70ddf9ac2c5a914aa3ce50eec
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2024-06-23 22:34:51 +0900

    [Gold V] Title: 빗물, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/14719. 빗물/빗물.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int H, W;
+const int MAX = 501;
+int A[MAX];
+
+stack<int> st;
+int res = 0;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> H >> W;
+    for (int i = 0; i < W; i++) {
+        cin >> A[i];
+    }
+    A[W++] = 0;
+
+    for (int i = 0; i <= W; i++) {
+        int k = 0;
+        while (!st.empty() && A[st.top()] < A[i]) {
+            res += (i - st.top() - 1) * (A[st.top()] - k);
+            k = A[st.top()];
+            st.pop();
+        }
+
+        if (!st.empty()) {
+            res += (i - st.top() - 1) * (A[i] - k);
+        }
+        st.push(i);
+    }
+
+    cout << res << '\n';
+    return 0;
+}
