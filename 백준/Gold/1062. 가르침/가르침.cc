703ef74e09c2be44703bcb1eb763c43259ee380c
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2024-06-27 02:57:46 +0900

    [Gold IV] Title: 가르침, Time: 44 ms, Memory: 5224 KB -BaekjoonHub

--- 백준/Gold/1062. 가르침/가르침.cc
+++ 백준/Gold/1062. 가르침/가르침.cc
@@ -9,6 +9,22 @@ vector<int> V;
 string antatica = "antatica";
 int P[26];
 
+void choose(int N, int K, vector<int> &V) {
+    vector<int> C;
+    for (int i = 0; i < N - K; i++) C.push_back(0);
+    for (int i = 0; i < K; i++) C.push_back(1);
+    do {
+        int newBits = 0;
+        for (char c : { 'a', 'n', 't', 'i', 'c' }) { 
+            newBits = newBits | (1 << (c - 'a')); 
+        }
+        for (int k = 0; k < N; k++) { 
+            if (C[k] == 1) newBits = newBits | (1 << P[k]); 
+        }
+        V.push_back(newBits);
+    } while (next_permutation(C.begin(), C.end()));
+}
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
@@ -28,25 +44,13 @@ int main() {
     
     // 알파벳 압축 복원
     int cur = 0;
-    for (int i = 0; i < 26; i++) {
-        if (antatica.find('a' + i) == string::npos) P[cur++] = i;
+    for (int i = 0; i < 26; i++) { 
+        if (antatica.find('a' + i) == string::npos) P[cur++] = i; 
     }
 
     // L개 중 K개 선택
-    for (int bits = 0; bits < (1 << L); bits++) {
-        int cnt = 0;
-        for (int k = 0; k < L; k++) { if (bits & (1 << k)) cnt++; }
-        if (cnt != K - 5) continue;
-        
-        int newBits = 0;
-        for (char c : { 'a', 'n', 't', 'i', 'c' }) {
-            newBits = newBits | (1 << (c - 'a'));
-        }
-        for (int k = 0; k < L; k++) { 
-            if (bits & (1 << k)) newBits = newBits | (1 << P[k]); 
-        }
-        V.push_back(newBits);
-    }
+    K -= 5;
+    if (K >= 0) choose(L, K, V);
 
     // 결과 출력
     int res = 0;
