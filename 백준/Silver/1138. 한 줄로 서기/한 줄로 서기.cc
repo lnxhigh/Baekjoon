4c6e0af6e7ab8bf524f0cc3c7496edbd6fb92cad
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2024-08-10 01:36:17 +0900

    [Silver II] Title: 한 줄로 서기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.cc
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.cc
@@ -3,49 +3,27 @@ using namespace std;
 
 int N;
 const int N_MAX = 1 << 4;
-int H[N_MAX];
-
-int L[N_MAX];
-bool isFinished = false;
-
-
-void f(int k, int s) {
-    if (isFinished) return;
-    
-    if (k == N) {
-        // Check if the height sequence is valid
-        for (int i = 0; i < N; i++) {
-            int cnt = 0;
-            for (int x = 0; x < i; x++) {
-                if (L[x] > L[i]) cnt++;
-            }
-            if (cnt != H[L[i]]) return;
-        }
-
-        // Finish
-        isFinished = true;
-        for (int i = 0; i < N; i++) {
-            cout << L[i] + 1 << ' ';
-        }
-        cout << '\n';
-        
-        return;
-    }
-
-    for (int i = 0; i < N; i++) {
-        // return if the person is already selected
-        // return if the selection is impossible
-        if ((s & (1 << i)) || k < H[i]) continue;
-        
-        // Mark as selected and proceed to next step
-        L[k] = i;
-        f(k + 1, s | (1 << i));
-    }
-}
+int H[N_MAX], L[N_MAX];
 
 int main() {
     cin >> N;
     for (int i = 0; i < N; i++) cin >> H[i];
-    f(0, 0);
+
+    memset(L, -1, sizeof(L));
+    for (int i = 0; i < N; i++) {
+        int loc = 0, cnt = 0;
+        for (int k = 0; k < N; k++) {
+            if (cnt == H[i]) { loc = k; break; }
+            if (L[k] == -1 || L[k] > i) cnt++;
+        }
+        for (int k = loc; k < N; k++) {
+            if (L[k] == -1) { L[k] = i; break; }
+        }
+    }
+
+    for (int i = 0; i < N; i++) {
+        cout << L[i] + 1 << ' ';
+    }
+    cout << '\n';
     return 0;
 }
