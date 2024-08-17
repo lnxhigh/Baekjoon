060cc0d1a1e832502a6fc95c1eba39964f9a5efe
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2024-08-17 21:05:42 +0900

    [Bronze III] Title: A+B - 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 10953: A+B - 6
+int TC;
+
+int main() {
+    cin >> TC;
+    while (TC--) {
+        int A, B;
+        char comma;
+        cin >> A >> comma >> B;
+        cout << A + B << '\n';
+    }
+
+    return 0;
+}
