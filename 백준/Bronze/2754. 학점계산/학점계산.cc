ad4b995dacc1d91dde508f0d2d94eb8b7bec25c9
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2024-08-17 21:14:54 +0900

    [Bronze III] Title: 학점계산, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+unordered_map<string,string> gp = {
+    {"A+", "4.3"}, {"A0", "4.0"}, {"A-", "3.7"},
+    {"B+", "3.3"}, {"B0", "3.0"}, {"B-", "2.7"},
+    {"C+", "2.3"}, {"C0", "2.0"}, {"C-", "1.7"},
+    {"D+", "1.3"}, {"D0", "1.0"}, {"D-", "0.7"},
+    {"F",  "0.0"}
+};
+
+int main() {
+    string grade, point;
+    cin >> grade;
+    point = gp[grade];
+    cout << point << '\n';
+    return 0;
+}
