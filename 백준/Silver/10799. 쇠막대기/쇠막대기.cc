aa18529e99acec8e8af9e0b6e87abf817beb08da
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2024-08-17 15:50:03 +0900

    [Silver II] Title: 쇠막대기, Time: 0 ms, Memory: 1240 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.cc
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.cc
@@ -1,16 +1,2 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-string S;
-int X, K, L;
-
-int main() {
-    cin >> S;
-    L = S.size();
-    for (int i = 0; i < L; i++) {
-        if (S[i] == '(') { K++; }
-        else { K--; X += (S[i-1] == '(') ? K : 1; }
-    }
-    cout << X << '\n';
-    return 0;
-}
+#include<cstdio>
+int X,K,i;char S[1<<17];int main(){scanf("%s",S);for(;S[i];i++)S[i]=='('?K++:X+=S[i-1]=='('?--K:K---K;printf("%d",X);}
