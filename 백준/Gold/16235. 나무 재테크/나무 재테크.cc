145b0a5f564aef33a1d30b07f1fb56a620f76b80
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2024-08-19 23:51:12 +0900

    [Gold III] Title: 나무 재테크, Time: 132 ms, Memory: 3052 KB -BaekjoonHub

--- 백준/Gold/16235. 나무 재테크/나무 재테크.cc
+++ 백준/Gold/16235. 나무 재테크/나무 재테크.cc
@@ -1,43 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Tree {
-    int x, y;
-    int age;
-};
+// No sort version 
 
 int N, M, K;
 const int MAX = 1 << 4;
 int A[MAX][MAX], B[MAX][MAX];
-vector<Tree> Forest[MAX][MAX];
+vector<int> InitForest[MAX][MAX];
+deque<int> Forest[MAX][MAX];
 
 int dx[8] = { -1, 0, +1, 0, -1, +1, +1, -1 };
 int dy[8] = { 0, -1, 0, +1, -1, -1, +1, +1 }; 
+
+void start() {
+    for (int x = 0; x < N; x++) {
+        for (int y = 0; y < N; y++) {
+            vector<int> &trees = InitForest[x][y];
+            sort(trees.begin(), trees.end());
+            
+            deque<int> dq;
+            for (const int &age : trees) {
+                dq.push_back(age);
+            }
+            Forest[x][y] = dq;
+        }
+    }
+}
 
 void update() {
     // Spring and Summer
     for (int x = 0; x < N; x++) {
         for (int y = 0; y < N; y++) {
-            vector<Tree> newTrees;
-            vector<Tree> &trees = Forest[x][y];
+            deque<int> newTrees;
+            deque<int> &trees = Forest[x][y];
             int &nourishment = B[x][y];
             int supplement = 0;
-            
-            // Eat from younger tree
-            sort(trees.begin(), trees.end(), [] (const Tree &left, const Tree &right) {
-                return left.age < right.age;
-            });
 
-            for (const Tree &tree : trees) {
-                if (nourishment >= tree.age) {
+            for (const int &age : trees) {
+                if (nourishment >= age) {
                     // Take in nourishment
-                    nourishment -= tree.age;
-                    Tree newTree = { x, y, tree.age+1 }; 
-                    newTrees.push_back(newTree);
+                    nourishment -= age;
+                    newTrees.push_back(age + 1);
                 }
                 else {
                     // Die
-                    supplement += tree.age / 2;
+                    supplement += age / 2;
                 }
             }
 
@@ -47,26 +54,27 @@ void update() {
     }
 
     // Fall
-    vector<Tree> newTrees;
+    vector<pair<int,int>> locs;
 
     for (int x = 0; x < N; x++) {
         for (int y = 0; y < N; y++) {
-            for (const Tree &tree : Forest[x][y]) {
-                if (tree.age % 5 != 0) continue;
+            for (const int &age : Forest[x][y]) {
+                if (age % 5 != 0) continue;
 
                 // Reproduce if the age is a multiple of five
                 for (int dir = 0; dir < 8; dir++) {
                     int nx = x + dx[dir], ny = y + dy[dir];
                     if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
-                    Tree newTree = { nx, ny, 1 };
-                    newTrees.push_back(newTree);
+                    locs.push_back({ nx, ny });
                 }
             }
         }
     }
 
-    for (const Tree &newTree : newTrees) {
-        Forest[newTree.x][newTree.y].push_back(newTree);
+    for (const pair<int,int> &loc : locs) {
+        int x = loc.first, y = loc.second;
+        int age = 1;
+        Forest[x][y].push_front(age);
     }
 
     // Winter
@@ -99,10 +107,10 @@ int main() {
     for (int i = 0; i < M; i++) {
         int x, y, z;
         cin >> x >> y >> z;
-        Tree tree = { --x, --y, z };
-        Forest[tree.x][tree.y].push_back(tree);
+        InitForest[--x][--y].push_back(z);
     }
 
+    start();
     for (int year = 0; year < K; year++) {
         update();
     }
