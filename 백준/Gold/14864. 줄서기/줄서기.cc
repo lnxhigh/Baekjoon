bfd996cf6f36dddee5a1cda67e7d8314a415f508
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2024-08-29 00:34:06 +0900

    [Gold II] Title: 줄서기, Time: 176 ms, Memory: 11748 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/14864. 줄서기/줄서기.cc
+++ 백준/Gold/14864. 줄서기/줄서기.cc
@@ -28,9 +28,9 @@ void update(int i, int diff) {
 
 int find(int nth) {
     int idx = 0, sum = 0;
-    int maxBit = 17;
-    
-    for (int mask = maxBit; mask > 0; mask >>= 1) {
+    if (nth > N) return -1;
+
+    for (int mask = N_MAX; mask > 0; mask >>= 1) {
         int nextIdx = idx + mask;
         if (nextIdx <= N && sum + tree[nextIdx] < nth) {
             sum += tree[nextIdx];
@@ -54,21 +54,21 @@ int main() {
         update(i, 1);
     }
 
+    bool check = true;
+
     for (int i = 1; i <= N; i++) {
         int k = find(A[i] + 1);
+        if (k == -1) { check = false; break; }
+
         res[i] = k;
         update(k, -1);
     }
 
-    bool check = true;
     for (int i = 0; i < M; i++) {
         int X = P[i].first, Y = P[i].second;
-        if (res[X] < res[Y]) {
-            check = false;
-            break;
-        }
+        if (res[X] <= res[Y]) { check = false; break; }
     }
-
+    
     if (!check) cout << -1;
     else for (int i = 1; i <= N; i++) { cout << res[i] << ' '; }
     cout << '\n';
