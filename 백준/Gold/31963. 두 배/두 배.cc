9b21dbb3b6d3946bf271a28bf3c4c9ee78d64eba
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2024-09-07 19:06:17 +0900

    [Gold IV] Title: 두 배, Time: 116 ms, Memory: 6116 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/31963. 두 배/두 배.cc
+++ 백준/Gold/31963. 두 배/두 배.cc
@@ -1,23 +1,44 @@
 #include <bits/stdc++.h>
 #define FastIO cin.tie(0)->sync_with_stdio(0);
 using namespace std;
+using ll = long long;
 
 int N;
+const int MAX = 1 << 18;
+ll A[MAX];
+ll counts[MAX];
+
 int main() {
     cin >> N;
-    int cur; cin >> cur;
+    ll cur; cin >> cur;
+    
+    for (int i = 1; i < N; i++) {
+        ll next; cin >> next;
 
-    int res = 0;
-    for (int i = 0; i < N-1; i++) {
-        int next; cin >> next;
-        while (cur > next) {
-            next <<= 1;
-            res++;
+        ll cnt = 0;
+        ll sign = cur > next ? 1 : cur < next ? -1 : 0;
+
+        bool same = false;
+        ll a = min(cur, next);
+        ll b = max(cur, next);
+
+        while (a < b) {
+            a <<= 1;
+            cnt += sign;
+            if (a == b) same = true;
         }
 
+        if (!same && cur < next) cnt++;
+
         cur = next;
+        counts[i] = max(0LL, counts[i-1] + cnt);
     }
 
+    long long res = 0;
+    for (int i = 0; i < N; i++) {
+        res += (long long) counts[i];
+    }
     cout << res << '\n';
+
     return 0;
 }
