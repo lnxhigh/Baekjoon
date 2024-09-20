b569d7acc20cfe55427577f8453c0a62cdf39316
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2024-09-21 07:11:52 +0900

    [Gold V] Title: 실질적 약수, Time: 4 ms, Memory: 3168 KB -BaekjoonHub

--- 백준/Gold/2247. 실질적 약수/실질적 약수.cc
+++ 백준/Gold/2247. 실질적 약수/실질적 약수.cc
@@ -1,22 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 using int64 = long long;
-const int MOD = 1000000;
-const int64 MOD64 = 1000000;
+const int64 MOD = 1000000;
+int64 N, X;
+
+int f(int64 N, int64 K) {
+    // Find all X satisfying [N / X] = K
+    // i.e. Find A <= X <= B such that 
+    // K * X <= N < (K+1) * X
+    int64 A = N / (K + 1) + 1;
+    int64 B = N / K;
+
+    // Return sum
+    int64 S = (B - A + 1) * (A + B) / 2;
+    return ((S % MOD) * (K % MOD)) % MOD;
+}
 
 int main() {
-    int N; cin >> N;
-    int X = 0;
-    for (int i = 1; i <= N; i++) {
-        X += i * (N / i);
+    cin >> N;
+    
+    // The possible values of [N / i] are limited
+    unordered_set<int64> counts;
+    for (int64 i = 1; i * i <= N; i++) {
+        counts.insert(i);
+        counts.insert(N / i);
+    }
+    
+    for (int64 k : counts) {
+        X += f(N, k);
         X %= MOD;
     }
 
-    int64 N64 = N, X64 = X + 1;
-    X64 -= N64 + N64 * (N64 + 1) / 2;
-    X64 %= MOD64;
-    if (X64 < 0) X64 += MOD64;
-
-    cout << X64 << '\n';
+    // 1 and N is not a divisor
+    X -= N;
+    X -= N * (N + 1) / 2;
+    X++;
+    
+    X %= MOD;
+    if (X < 0) X += MOD;
+    cout << X << '\n';
     return 0;
 }
