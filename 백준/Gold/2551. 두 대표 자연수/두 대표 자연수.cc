c11917760dd6bb8b52b428c687375125ba2d2287
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2024-09-22 14:02:32 +0900

    [Gold IV] Title: 두 대표 자연수, Time: 320 ms, Memory: 2100 KB -BaekjoonHub

--- 백준/Gold/2551. 두 대표 자연수/두 대표 자연수.cc
+++ 백준/Gold/2551. 두 대표 자연수/두 대표 자연수.cc
@@ -2,25 +2,34 @@
 #define FastIO cin.tie(0)->sync_with_stdio(0);
 using namespace std;
 using int64 = long long;
+const int MAX = 10001;
+int64 A[MAX];
 
 int main() {
     FastIO
     
     int N; cin >> N;
-    vector<int64> A(N);
-    for (int64 &x : A) cin >> x;
-    
+    for (int i = 0; i < N; i++) {
+        int64 X; cin >> X; A[X]++;
+    }
+
     // Median
-    sort(A.begin(), A.end());
-    int64 median = A[(N-1) / 2];
+    int64 median = 0;
+    int64 K = (N + 1) / 2;
+
+    int64 cur = 0;
+    for (int i = 0; i < MAX; i++) {
+        int64 next = cur + A[i];
+        if (K > cur && K <= next) { median = i; break; }
+        cur = next;
+    }
 
     // Mean
-    int64 sum = accumulate(A.begin(), A.end(), 0LL);
-    int64 cnt = N;
+    int64 sum = 0, cnt = N;
+    for (int64 i = 0; i < MAX; i++) { sum += i * A[i]; }
     int64 mean = sum / cnt;
     if ((sum % cnt) > (cnt / 2)) mean++;
-    
-    // Result
+
     cout << median << ' ' << mean << '\n';
     return 0;
 }
