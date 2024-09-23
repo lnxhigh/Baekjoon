2b4f016c96ab03a4b83d84764f896eea109a55d4
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2024-09-23 14:59:16 +0900

    [Gold II] Title: 기지국, Time: 148 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Gold/2300. 기지국/기지국.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define X first
+#define Y second
+using namespace std;
+using Point = pair<int,int>;
+
+int N;
+const int INF = 1 << 20;
+
+int main() {
+    cin >> N;
+    vector<Point> P(N);
+    for (auto &[x,y] : P) { cin >> x >> y; }
+    sort(P.begin(), P.end());
+
+    vector<int> D(N+1, INF);
+    D[0] = 0;
+
+    for (int i = 1; i <= N; i++) {
+        vector<int> H(i+1);
+        for (int k = i-1; k >= 0; k--) {
+            H[k] = max(H[k+1], abs(P[k].Y));
+        }
+
+        for (int k = 0; k < i; k++) {
+            int len = max(P[i-1].X - P[k].X, H[k] << 1);
+            D[i] = min(D[i], D[k] + len);
+        }
+    }
+
+    cout << D[N] << '\n';
+    return 0;
+}
