370a769ca3d9f848dce994f6d130c658f70f54ba
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2024-09-24 10:38:40 +0900

    [Silver I] Title: 동물원, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.cc
+++ 백준/Silver/1309. 동물원/동물원.cc
@@ -1,23 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
+using Matrix = vector<vector<int>>;
+const int MOD = 9901;
 
-int N;
-const int N_MAX = 100001;
-const int DIV = 9901;
+// O(log N) solution with matrix
 
-int D[N_MAX][2][2];
+const int D = 3;
+const Matrix A = {
+    {1, 1, 1}, 
+    {1, 0, 1}, 
+    {1, 1, 0}
+};
 
-int main() {
-    cin >> N;
+Matrix operator*(const Matrix &A, const Matrix &B) {
+    Matrix X = Matrix(D, vector<int>(D, 0));
 
-    D[0][0][0] = 1;
-    for (int i = 0; i < N; i++) {
-        D[i+1][0][0] = (D[i][0][0] + D[i][0][1] + D[i][1][0]) % DIV;
-        D[i+1][0][1] = (D[i][0][0] + D[i][1][0]) % DIV;
-        D[i+1][1][0] = (D[i][0][0] + D[i][0][1]) % DIV;
+    for (int r = 0; r < D; r++) {
+        for (int c = 0; c < D; c++) {
+            for (int k = 0; k < D; k++) {
+                X[r][c] += A[r][k] * B[k][c];
+                X[r][c] %= MOD;
+            }
+        }
     }
 
-    int res = (D[N][0][0] + D[N][0][1] + D[N][1][0]) % DIV;
+    return X;
+}
+
+Matrix operator^(const Matrix &X, int p) {
+    if (p == 0) {
+        Matrix I = Matrix(D, vector<int>(D, 0));
+        for (int i = 0; i < D; i++) I[i][i] = 1;
+        return I;
+    }
+
+    Matrix S = X ^ (p / 2);
+    Matrix Y = S * S;
+    if (p & 1) Y = X * Y;
+    
+    return Y;
+}
+
+int main() {
+    int N; cin >> N;
+    Matrix X = A ^ N;
+
+    int res = X[0][0] + X[1][0] + X[2][0];
+    res %= MOD;
     cout << res << '\n';
+    
     return 0;
 }
