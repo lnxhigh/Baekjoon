caae5d5e25a9dbf8fdb738fa8ff9ad0eb2729b1f
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2024-09-25 18:21:19 +0900

    [Gold I] Title: 세진이의 미팅, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/15791. 세진이의 미팅/세진이의 미팅.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+using int64 = long long;
+const int64 MOD = 1e9 + 7;
+
+int64 power(int64 x, int64 p) {
+    int64 res = 1, mul = x;
+    while (p) {
+        if (p & 1) res = (res * mul) % MOD;
+        mul = (mul * mul) % MOD;
+        p >>= 1;
+    }
+    return res;
+}
+
+int main() {
+    int64 N, M; cin >> N >> M;
+
+    int64 res = 1, mul = 1;
+    for (int64 i = 0; i < M; i++) {
+        res = (res * (N - i)) % MOD;
+        mul = (mul * (i + 1)) % MOD;
+    }
+
+    int64 inv = power(mul, MOD - 2);
+    res = (res * inv) % MOD;
+    cout << res << '\n';
+    return 0;
+}
