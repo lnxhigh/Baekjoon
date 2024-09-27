2ff3e1fb1116400ac7f4eeca6d7bad948f07c561
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2024-09-27 21:46:07 +0900

    [Gold III] Title: ㄷㄷㄷㅈ, Time: 312 ms, Memory: 20476 KB -BaekjoonHub

---
+++ 백준/Gold/19535. ㄷㄷㄷㅈ/ㄷㄷㄷㅈ.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#define FastIO cin.tie(0)->sync_with_stdio(0);
+using namespace std;
+using int64 = long long;
+
+int64 D(const vector<vector<int>> &tree, const int N) {
+    int64 res = 0;
+    for (int i = 0; i < N; i++) {
+        for (int j : tree[i]) {
+            int64 x = (int64) tree[i].size();
+            int64 y = (int64) tree[j].size();
+            res += (x - 1) * (y - 1);
+        }
+    }
+    
+    return res / 2;
+}
+
+int64 G(const vector<vector<int>> &tree, const int N) {
+    int64 res = 0;
+    for (int i = 0; i < N; i++) {
+        int64 k = (int64) tree[i].size();
+        res += k * (k-1) * (k-2) / 6;
+    }
+    
+    return res;
+}
+
+int main() {
+    int N; cin >> N;
+    vector<vector<int>> tree(N);
+
+    for (int i = 0; i < N-1; i++) {
+        int u, v; cin >> u >> v;
+        --u, --v;
+        tree[u].push_back(v);
+        tree[v].push_back(u);
+    }
+
+    int64 d = D(tree, N);
+    int64 g = G(tree, N);
+    
+    string res = (d > 3*g) ? "D" : (d < 3*g) ? "G" : "DUDUDUNGA";
+    cout << res << '\n';
+    return 0;
+}
