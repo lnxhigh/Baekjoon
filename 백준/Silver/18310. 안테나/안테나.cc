8ed779ede08e1508ca4d2c65d676a1ae16a075db
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2024-09-29 00:41:17 +0900

    [Silver III] Title: 안테나, Time: 32 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Silver/18310. 안테나/안테나.cc
+++ 백준/Silver/18310. 안테나/안테나.cc
@@ -1,7 +1,9 @@
 #include <bits/stdc++.h>
+#define FastIO cin.tie(0)->sync_with_stdio(0);
 using namespace std;
 
 int main() {
+    FastIO
     int N; cin >> N;
     vector<int> A(N);
     for (int &x : A) cin >> x;
