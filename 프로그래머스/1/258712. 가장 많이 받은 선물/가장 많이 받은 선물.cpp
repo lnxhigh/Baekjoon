c820c7e2a3a8aacd48a571aba3d7ff79d430a9f7
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2024-09-29 21:41:57 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 6.47 ms, Memory: 4.62 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.cpp
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.cpp
@@ -52,20 +52,4 @@ int solution(vector<string> friends, vector<string> gifts) {
     int answer = *max_element(gift_expected, gift_expected + N);
     
     return answer;
-    
-    
-    
-    /*
-    누가 선물을 많이 받을지
-    선물을 주고받은 기록이 있다면 더 많은 선물을 준 사람이 다음달에 받는다
-        
-        기록이 없거나 주고받은수가 같다면 선물지수가 더큰사람이 작은 사람에게 선물으받는다
-        선물지수는 준선물수 - 받은선물수 
-        선물지수까지 같으면 선물을 주고받지 않는다
-        그래프문제삘
-        
-        선물을가장많이받을 친구의 선물수
-     */  
-        
-        
 }
