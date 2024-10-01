09b339053fc39f97b9e0eaa01ce058aa1d851985
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2024-10-01 22:09:01 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 312 ms, Memory: 25980 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAX = 100000;
+set<int,greater<int>> graph[MAX];
+int visited[MAX];
+int T = 0;
+
+void bfs(int start) {
+    visited[start] = ++T;
+    
+    queue<int> q;
+    q.push(start);
+
+    while (!q.empty()) {
+        int cur = q.front();
+        q.pop();
+        for (int next : graph[cur]) {
+            if (!visited[next]) {
+                visited[next] = ++T;
+                q.push(next);
+            }
+        }
+    }
+}
+
+int main() {
+    int V, E, R;
+    cin >> V >> E >> R;
+
+    for (int i = 0; i < E; i++) {
+        int u, v; cin >> u >> v;
+        --u; --v;
+        graph[u].insert(v);
+        graph[v].insert(u);
+    }
+
+    bfs(--R);
+
+    for (int i = 0; i < V; i++) {
+        cout << visited[i] << '\n';
+    }
+
+    return 0;
+}
