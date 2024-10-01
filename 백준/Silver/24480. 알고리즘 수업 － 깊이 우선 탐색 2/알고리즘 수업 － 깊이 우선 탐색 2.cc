968028de3ac5a5d6941654e6fac5b6e43c988a8d
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2024-10-01 22:02:13 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 392 ms, Memory: 26516 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAX = 100000;
+set<int,greater<int>> graph[MAX];
+int visited[MAX];
+int T = 0;
+
+void dfs(int cur) {
+    visited[cur] = ++T;
+    for (int next : graph[cur]) {
+        if (!visited[next]) dfs(next);
+    }
+}
+
+int main() {
+    int V, E, R;
+    cin >> V >> E >> R;
+
+    for (int i = 0; i < E; i++) {
+        int u, v; cin >> u >> v;
+        --u; --v;
+        graph[u].insert(v);
+        graph[v].insert(u);
+    }
+
+    dfs(--R);
+
+    for (int i = 0; i < V; i++) {
+        cout << visited[i] << '\n';
+    }
+
+    return 0;
+}
