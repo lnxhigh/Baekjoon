6fbf7fd40f608cc8311c518c45a4956815ce28b6
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2024-10-01 21:26:02 +0900

    [Silver I] Title: 지름길, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Silver/1446. 지름길/지름길.cc
+++ 백준/Silver/1446. 지름길/지름길.cc
@@ -1,54 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
+const int MAX = 10001, INF = 1 << 20;
 
-const int MAX = 10001;
-const int INF = 1 << 20;
-vector<pair<int,int>> graph[MAX];
+vector<tuple<int,int,int>> paths;
 int D[MAX];
 
 int main() {
     int N, K; cin >> N >> K;
-    
-    vector<int> V;
-    V.push_back(0); V.push_back(K);
-    
     for (int i = 0; i < N; i++) {
-        int s, e, l; 
-        cin >> s >> e >> l;
-        if (e > K || l >= e - s) continue;
-        
-        graph[s].push_back({ l, e });
-        V.push_back(s); V.push_back(e);
-    }
-
-    for (int i : V) {
-        for (int j : V) {
-            if (i < j) graph[i].push_back({ j - i, j });
-        }
+        int from, to, len; 
+        cin >> from >> to >> len;
+        if (to > K || len >= to - from) continue;
+        auto t = tie(from, to, len);
+        paths.push_back(t);
     }
     
     fill(D, D + MAX, INF);
     D[0] = 0;
-
-    priority_queue<pair<int,int>> pq;
-    pq.push({ 0, 0 });
-
-    while (!pq.empty()) {
-        int cur = pq.top().second;
-        int cost = -pq.top().first;
-        pq.pop();
-
-        if (D[cur] < cost) continue;
-
-        for (auto [ weight, next ] : graph[cur]) {
-            int newCost = cost + weight;
-            if (D[next] > newCost) {
-                D[next] = newCost;
-                pq.push({ -newCost, next });
-            }
+    
+    for (int i = 1; i <= K; i++) {
+        D[i] = D[i-1] + 1;
+        for (const auto& [ from, to, len ] : paths) {
+            if (to == i) D[i] = min(D[i], D[from] + len);
         }
     }
-
+    
     cout << D[K] << '\n';
     return 0;
 }
