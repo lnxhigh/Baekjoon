ef75e38df4a1f5617071dcaa77d9d0a113e5e926
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2024-10-02 21:36:23 +0900

    [Silver II] Title: 마인크래프트, Time: 308 ms, Memory: 2996 KB -BaekjoonHub

---
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.cc
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int MAX = 500;
+
+int H[MAX][MAX];
+
+int main() {
+    int R, C; cin >> R >> C;
+    int B; cin >> B;
+
+    int M = 0, m = 1 << 8;
+    for (int i = 0; i < R; i++) {
+        for (int j = 0; j < C; j++) {
+            cin >> H[i][j];
+            M = max(M, H[i][j]);
+            m = min(m, H[i][j]);
+        }
+    }
+
+    int minTime = 1 << 30, height = -1;
+    for (int h = m; h <= M; h++) {
+        int t = 0, cnt = B;
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                if (H[i][j] > h) {
+                    cnt += H[i][j] - h;
+                    t += (H[i][j] - h) << 1;
+                }
+            }
+        }
+        
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                if (H[i][j] < h) {
+                    cnt -= h - H[i][j];
+                    t += h - H[i][j];
+                }
+            }
+        }
+
+        if (cnt < 0) continue;
+
+        if (t <= minTime) {
+            minTime = t;
+            height = h;
+        }
+    }
+
+    cout << minTime << ' ' << height << '\n';
+    return 0;
+}
