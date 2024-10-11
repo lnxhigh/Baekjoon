8c2d0938fb4f8fa09ba530bf30f7c0641f9c6f73
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2024-10-11 14:38:29 +0900

    [Silver I] Title: 카잉 달력, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/6064. 카잉 달력/카잉 달력.cc
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.cc
@@ -10,31 +10,12 @@ tuple<int64,int64,int64> euclidean(int64 a, int64 b) {
     return { g, y, x - (a / b) * y };
 }
 
-int64 inverse(int64 a, int64 b) {
-    auto [ g, x, y ] = euclidean(a, b);
-    return (x % b + b) % b;
-}
-
-int64 crt(const vector<pair<int,int>> &info) {
-    int64 M = 1;
-    for (const auto& [i, r] : info) M *= i;
-
-    int64 res = 0;
-    for (const auto& [i, r] : info) {
-        int64 Mi = M / i;
-        int64 yi = inverse(Mi, i);
-        res += Mi * yi * r;
-    }
-
-    return res % M;
-}
-
 int64 solve(int64 M, int64 N, int64 x, int64 y) {
     --x, --y;
     auto [g, k0, _] = euclidean(M, N);
     if (x % g != y % g) return -1;
     
-    k0 *= (y - x) / g; // 특수해
+    k0 *= (y - x) / g;
     int k = (M * k0 + x) % (M * N / g);
     if (k < 0) k += M * N / g;
     return ++k;
@@ -48,4 +29,6 @@ int main() {
         cin >> M >> N >> x >> y;
         cout << solve(M, N, x, y) << '\n';
     }
+
+    return 0;
 }
