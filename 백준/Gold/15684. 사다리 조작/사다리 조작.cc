25122e5b9ff3e1db7baab14e3b444e5cf5a6d668
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2024-11-13 09:16:51 +0900

    [Gold III] Title: 사다리 조작, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/15684. 사다리 조작/사다리 조작.cc
+++ 백준/Gold/15684. 사다리 조작/사다리 조작.cc
@@ -19,7 +19,7 @@ bool check() {
     return true;
 }
 
-void dfs(int x, int y, int depth, int cnt) {
+void dfs(int depth, int cnt) {
     if (depth == cnt) {
         if (check()) {
             finish = true;
@@ -28,26 +28,27 @@ void dfs(int x, int y, int depth, int cnt) {
         return;
     }
 
-    // (x, y) 에서 탐색을 시작
-    for (int k = y; k < N; k++) {
+    for (int k = 1; k < N; k++) {
         if (finish) return;
-        int s = (k == y) ? x : 0;
 
-        for (int i = s; i <= H; i++) {
+        for (int i = 1; i <= H; i++) {
             if (finish) return;
 
             // 가로선을 놓을 수 있는지 검사한다
             if (X[i][k - 1] || X[i][k] || X[i][k + 1]) continue;
 
             X[i][k] = true;
-            dfs(i + 1, k, depth + 1, cnt);
+            dfs(depth + 1, cnt);
             X[i][k] = false;
+            
+            // 이 부분 넣게 되면 커버하지 못하는 부분이 생기는듯 ...
+            // (x, y) 에서 시작하는 것이 아닌
+            // 전체탐색으로 바꾸면 같은 위상을 만들어낼 수 있지 않을까
 
-            // 
-            // while (i <= H) {
-            //     if (X[i][k - 1] || X[i][k + 1]) break;
-            //     i++;
-            // }
+            while (i <= H) {
+                if (X[i][k - 1] || X[i][k + 1]) break;
+                i++;
+            }
         }
     }
 }
@@ -59,7 +60,7 @@ int main() {
         X[a][b] = true;
     }
 
-    for (int i = 0; i < 4; i++) dfs(1, 1, 0, i);
+    for (int i = 0; i < 4; i++) dfs(0, i);
     if (ans >= 4) ans = -1;
     cout << ans << '\n';
     return 0;
