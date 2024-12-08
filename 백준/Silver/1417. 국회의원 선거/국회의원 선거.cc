c805afe91fffd19138e8cfff3030a359adff03d6
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2024-12-08 21:42:00 +0900

    [Silver V] Title: 국회의원 선거, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1417. 국회의원 선거/국회의원 선거.cc
+++ 백준/Silver/1417. 국회의원 선거/국회의원 선거.cc
@@ -1,40 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N;
-const int N_MAX = 1 << 8;
-int X[N_MAX];
-
 int main() {
-    cin >> N;
-    for (int i = 0; i < N; i++) {
-        cin >> X[i];
+    int N; cin >> N;
+    priority_queue<int> pq;
+
+    int cnt = 0;
+    int vote; cin >> vote;
+    for (int i = 1; i < N; i++) {
+        int x; cin >> x;
+        pq.push(x);
     }
 
-    int init = X[0];
-    
-    while (true) {
-        bool win = true;
-        for (int i = 1; i < N; i++) {
-            if (X[0] <= X[i]) {
-                win = false;
-                break;
-            }
-        }
-        if (win) break;
+    while (!pq.empty()) {
+        int x = pq.top();
+        pq.pop();
+        
+        if (vote + cnt > x) break;
+        cnt++;
 
-        int maxIdx = -1;
-        for (int i = 1; i < N; i++) {
-            if (maxIdx == -1 || X[i] > X[maxIdx]) {
-                maxIdx = i;
-            }
-        }
-
-        X[maxIdx]--;
-        X[0]++;
+        if (--x) pq.push(x);
     }
 
-    int ans = X[0] - init;
-    cout << ans << '\n';
+    cout << cnt << '\n';
     return 0;
 }
