3a73f80c80adbf25c48256a7de5cddb5d8db7d21
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2024-12-11 14:34:50 +0900

    [Gold I] Title: 그래프 트리 분할, Time: 36 ms, Memory: 4760 KB -BaekjoonHub

--- 백준/Gold/22954. 그래프 트리 분할/그래프 트리 분할.cc
+++ 백준/Gold/22954. 그래프 트리 분할/그래프 트리 분할.cc
@@ -6,118 +6,111 @@ const int MAX = 100000;
 int V, E;
 pair<int,int> Edges[MAX << 1];
 
-vector<int> graph[MAX];
-vector<int> tree[MAX];
-bool visited[MAX];
+int P[MAX];
+int D[MAX];
+vector<int> T;
 
-vector<int> fail = { -2 };
+int find(int x) {
+    if (x == P[x]) return x;
+    return P[x] = find(P[x]);
+}
 
-void print(vector<int> &vec) {
-    for (int i : vec) cout << i + 1 << ' ';
-    cout << '\n';
+bool merge(int x, int y) {
+    x = find(x), y = find(y);
+    if (x == y) return false;
+
+    x > y ? P[x] = y : P[y] = x;
+    return true;
 }
 
 void input() {
     cin >> V >> E;
+    T.reserve(V);
 
     for (int i = 0; i < E; i++) {
         int x, y; cin >> x >> y; --x, --y;
         Edges[i] = { x, y };
-        graph[x].push_back(i);
-        graph[y].push_back(i);
     }
-}
-
-void dfs(int cur, vector<int> &v, vector<int> &e) {
-    visited[cur] = true;
-    v.push_back(cur);
-
-    for (int i : graph[cur]) {
-        auto [x, y] = Edges[i];
-        int nxt = (x != cur) ? x : y;
-
-        if (!visited[nxt]) {
-            tree[cur].push_back(i);
-            e.push_back(i);
-            dfs(nxt, v, e);
-        }
-    }
-}
-
-pair<int,int> fall(vector<int> &v, vector<int> &e) {
-    int leaf = -1;
-    for (int i : v) {
-        if (tree[i].empty()) {
-            leaf = i;
-            break;
-        }
-    }
-    
-    int edge = -1;
-    for (int i : e) {
-        auto [x, y] = Edges[i];
-        if (x == leaf || y == leaf) {
-            edge = i;
-            break;
-        }
-    }
-
-    return { leaf, edge };
-}
-
-void remove_value(vector<int> &vec, int value) {
-    vec.erase(remove(vec.begin(), vec.end(), value), vec.end());
 }
 
 void solve() {
-    if (V <= 2) { 
-        print(fail);
-        return; 
+    // Union-Find
+    iota(P, P + V, 0);
+
+    int cnt = V;
+    for (int i = 0; i < E; i++) {
+        auto [x, y] = Edges[i];
+        
+        bool isMerged = merge(x, y);
+        if (!isMerged) continue;
+        
+        D[x]++, D[y]++;
+        T.push_back(i);
+        --cnt;
     }
 
-    vector<vector<int>> vs, es;
-    for (int i = 0; i < V; i++) {
-        if (visited[i]) continue;
-
-        vector<int> v, e;
-        dfs(i, v, e);
-        vs.push_back(v);
-        es.push_back(e);
-    }
-    int cnt = vs.size();
-
-    if (cnt >= 3) {
-        print(fail); 
+    // Exception
+    if (V <= 2 || cnt >= 3) {
+        cout << -1 << '\n';
         return;
     }
-    else if (cnt == 2) {
-        if (vs[0].size() == vs[1].size()) {
-            print(fail); 
-            return;
+
+    // Find leaf
+    int leaf = -1;
+    if (cnt == 1) {
+        for (int i = 1; i < V; i++) {
+            if (D[i] <= 1) leaf = i;
         }
+        
+        assert(leaf != -1);
+        fill(P, P + V, 0);
+        P[leaf] = leaf;
     }
-    else if (cnt == 1) {
-        auto [leaf, edge] = fall(vs[0], es[0]);
-        assert(leaf != -1 && edge != -1);
 
-        remove_value(vs[0], leaf);
-        remove_value(es[0], edge);
-
-        vector<int> newv = { leaf };
-        vector<int> newe;
-
-        vs.push_back(newv);
-        es.push_back(newe);
+    // Measure the size
+    int zero = 0, one = 0;
+    for (int i = 0; i < V; i++) {
+        if (find(i) == 0) zero++;
+        else one++;
     }
     
-    assert((int) vs[0].size() + (int) vs[1].size() == V);
-    cout << vs[0].size() << ' ' << vs[1].size() << '\n';
-    
-    assert((int) vs[0].size() - 1 == (int) es[0].size());
-    print(vs[0]); print(es[0]);
+    if (zero == one) {
+        cout << -1 << '\n';
+        return;
+    }
 
-    assert((int) vs[1].size() - 1 == (int) es[1].size());
-    print(vs[1]); print(es[1]);
+    // 1
+    cout << zero << ' ' << one << '\n';
+
+    // 2
+    for (int i = 0; i < V; i++) {
+        if (find(i) == 0) cout << i + 1 << ' ';
+    }
+    cout << '\n';
+
+    // 3
+    for (int i : T) {
+        auto [x, y] = Edges[i];
+        if (x == leaf || y == leaf) continue;
+        if (find(x) == 0) cout << i + 1 << ' ';
+    }
+    cout << '\n';
+
+    // 4
+    for (int i = 0; i < V; i++) {
+        if (find(i) != 0) cout << i + 1 << ' ';
+    }
+    cout << '\n';
+
+    // 5
+    for (int i : T) {
+        auto [x, y] = Edges[i];
+        if (x == leaf || y == leaf) continue;
+        if (find(x) != 0) cout << i + 1 << ' ';
+    }
+    cout << '\n';
+
+    return;
 }
 
 int main() {
