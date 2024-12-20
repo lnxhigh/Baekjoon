8f5a2459b751a92ad9ccfd197aeb04311f3068d6
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2024-12-21 02:53:47 +0900

    [Gold V] Title: 시간을 돌리고 싶어, Time: 16 ms, Memory: 2216 KB -BaekjoonHub

--- 백준/Gold/32347. 시간을 돌리고 싶어/시간을 돌리고 싶어.cc
+++ 백준/Gold/32347. 시간을 돌리고 싶어/시간을 돌리고 싶어.cc
@@ -5,52 +5,35 @@ const int MAX = 200001;
 
 int N, K;
 bool A[MAX];
-int  D[MAX];
 
 bool f(int t) {
     int cur = N - 1;
-    int cnt = 0;
-    
-    while (cur && A[cur]) {
-        cnt++;
-        int nxt = max(cur - t, 0);
-        nxt = D[nxt];
-        if (nxt >= cur) return false;
+    int cnt = K;
+
+    while (cnt--) {
+        int nxt = cur - t;
+        if (nxt <= 0) return true;
+        while (!A[nxt]) nxt++;
+        if (nxt == cur) return false;
         cur = nxt;
     }
 
-    return cur == 0 && cnt <= K;
+    return false;
 }
 
 int main() {
     FastIO
     cin >> N >> K;
-
     for (int i = 0; i < N; i++) {
         cin >> A[i];
-    }
-    A[0] = true;
-
-    stack<int> st;
-    fill(D, D + N, N);
-    for (int i = 0; i < N; i++) {
-        if (!A[i]) { st.push(i); continue; }
-        
-        D[i] = i;
-        while (!st.empty()) { D[st.top()] = i; st.pop(); }
     }
 
     int ans = -1;
     int low = 1, high = N - 1;
     while (low <= high) {
         int mid = (low + high) / 2;
-        if (f(mid)) {
-            ans = mid;
-            high = mid - 1;
-        }
-        else {
-            low = mid + 1;
-        }
+        if (f(mid)) ans = mid, high = mid - 1;
+        else low = mid + 1;
     }
 
     cout << ans << '\n';
