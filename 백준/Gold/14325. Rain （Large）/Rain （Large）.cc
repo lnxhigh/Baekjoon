d9993909c4d23d130792b8e1295c86dbd8c2badc
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2024-12-23 05:00:17 +0900

    [Gold I] Title: Rain (Large), Time: 20 ms, Memory: 2168 KB -BaekjoonHub

--- 백준/Gold/14325. Rain （Large）/Rain （Large）.cc
+++ 백준/Gold/14325. Rain （Large）/Rain （Large）.cc
@@ -1,16 +1,12 @@
 #include <bits/stdc++.h>
 #define FastIO cin.tie(0)->sync_with_stdio(0);
 using namespace std;
-using Point = pair<int,int>;
 const int MAX = 50;
 
 int TC;
 
 int R, C;
 int H[MAX][MAX];
-
-int T[MAX][MAX];
-bool V[MAX][MAX];
 int dx[4] = { -1, 0, +1, 0 };
 int dy[4] = { 0, -1, 0, +1 };
 
@@ -25,67 +21,44 @@ void input() {
     }
 }
 
-bool bfs(int x, int y, int level) {
+bool isEdge(int x, int y) {
+    return x == 0 || x == R - 1 || y == 0 || y == C - 1;
+}
+
+bool isInside(int x, int y) {
+    return x >= 0 && x < R && y >= 0 && y < C;
+}
+
+int bfs() {
     // Initialize
+    int ret = 0;
+    priority_queue<tuple<int,int,int>> pq;
+    bool V[MAX][MAX];
+
     for (int i = 0; i < R; i++) {
         for (int j = 0; j < C; j++) {
-            T[i][j] = max(H[i][j], level);
+            V[i][j] = isEdge(i, j);
+            if (V[i][j]) pq.push({ -H[i][j], i, j });
         }
     }
-    memset(V, false, sizeof(V));
 
-    // BFS
-    queue<Point> q;
-    q.push({ x, y });
-    V[x][y] = true;
-
-    while (!q.empty()) {
-        auto [cx, cy] = q.front();
-        q.pop();
-
-        if (cx == 0 || cx == R - 1 || cy == 0 || cy == C - 1) {
-            return true;
-        }
+    while (!pq.empty()) {
+        auto [height, cx, cy] = pq.top();
+        height = -height;
+        pq.pop();
 
         for (int i = 0; i < 4; i++) {
             int nx = cx + dx[i], ny = cy + dy[i];
-            if (T[nx][ny] > T[x][y] || V[nx][ny]) continue;
+            if (!isInside(nx, ny) || V[nx][ny]) continue;
+            
+            int diff = max(height - H[nx][ny], 0);
+            ret += diff;
 
-            q.push({ nx, ny });
+            pq.push({ -max(height, H[nx][ny]), nx, ny });
             V[nx][ny] = true;
         }
     }
 
-    return false;
-}
-
-int search(int x, int y) {
-    int level = H[x][y];
-    int low = H[x][y], high = 1000;
-
-    while (low <= high) {
-        int mid = (low + high) / 2;
-
-        if (bfs(x, y, mid)) {
-            level = mid;
-            high = mid - 1;
-        }
-        else {
-            low = mid + 1;
-        }
-    }
-
-    int diff = level - H[x][y];
-    return diff;
-}
-
-int solve() {
-    int ret = 0;
-    for (int i = 0; i < R; i++) {
-        for (int j = 0; j < C; j++) {
-            ret += search(i, j);
-        }
-    }
     return ret;
 }
 
@@ -94,7 +67,7 @@ int main() {
     cin >> TC;
     for (int tc = 1; tc <= TC; tc++) {
         input();
-        int ans = solve();
+        int ans = bfs();
         cout << "Case #" << tc << ": " << ans << "\n";
     }
 
