2914e4c5db8326f113a9c8a3edd792a2b1ea8fb0
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2024-12-29 15:46:31 +0900

    [Gold III] Title: 공주님의 정원, Time: 24 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/2457. 공주님의 정원/공주님의 정원.cc
+++ 백준/Gold/2457. 공주님의 정원/공주님의 정원.cc
@@ -1,55 +1,36 @@
 #include <bits/stdc++.h>
 #define FastIO cin.tie(0)->sync_with_stdio(0);
 using namespace std;
-const int MAX = 100005;
-using Date = pair<int,int>;
 
 int N;
-pair<Date,Date> A[MAX];
+int D[1234];
 
-void input() {
+void init() {
     cin >> N;
     for (int i = 0; i < N; i++) {
-        int m, d; 
-        cin >> m >> d; 
-        Date s = { m, d };
-        cin >> m >> d; 
-        Date e = { m, d };
-        
-        A[i] = { s, e };
+        int m1, d1, m2, d2; 
+        cin >> m1 >> d1 >> m2 >> d2;
+        int x = 100 * m1 + d1, y = 100 * m2 + d2;
+        D[x] = max(D[x], y);
     }
-    sort(A, A + N);
+
+    for (int i = 1; i <= 1231; i++) {
+        D[i] = max(D[i], D[i - 1]);
+    }
 }
 
 int solve() {
-    int cnt = 0, idx = 0;
-    Date first = { 3, 1 }, last = { 12, 1 };
-    Date cur = first, nxt = first;
-
-    while (idx < N) {
-        while (idx < N) {
-            auto [x, y] = A[idx];
-            if (x > cur) break;
-
-            nxt = max(nxt, y);
-            idx++;
-        }
-        if (nxt >= last) return ++cnt;
-        if (idx >= N) break;
-        
-        auto [x, y] = A[idx];
-        if (x > nxt) return 0;
-
-        cnt++;
-        cur = nxt;
+    int cnt = 0, x = 301;
+    while (x < 1201) {
+        if (D[x] <= x) break;
+        x = D[x], cnt++;
     }
-    
-    return 0;
+    return x >= 1201 ? cnt : 0;
 }
 
 int main() {
     FastIO
-    input();
+    init();
     int ans = solve();
     cout << ans << '\n';
     return 0;
