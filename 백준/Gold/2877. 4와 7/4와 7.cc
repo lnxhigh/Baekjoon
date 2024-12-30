9d871c8551bda4fd00f16c7e1487a1c3bc8a4dfa
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2024-12-30 12:32:11 +0900

    [Gold V] Title: 4와 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2877. 4와 7/4와 7.cc
+++ 백준/Gold/2877. 4와 7/4와 7.cc
@@ -1,21 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string f(int x) {
-    if (x == 1) return "";
-    
-    int i = 30;
-    while (!(x >> i & 1)) i--;
-
-    int j = i - 1;
-    string p = (x >> j & 1) ? "7" : "4";
-    
-    x &= ~(1 << i), x |= (1 << j);
-    return p + f(x);
-}
-
 int main() {
     int K; cin >> K;
-    cout << f(++K) << '\n';
-    return 0;
+    int i = 0;
+    while (K >= (1 << i)) K -= 1 << i++;
+    while (i--) cout << (K >> i & 1 ? '7' : '4');
 }
