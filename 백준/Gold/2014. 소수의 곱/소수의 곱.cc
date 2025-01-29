a913d803cdf10198f8a401ff16861c75fa397007
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2025-01-30 08:25:43 +0900

    [Gold I] Title: 소수의 곱, Time: 248 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/2014. 소수의 곱/소수의 곱.cc
+++ 백준/Gold/2014. 소수의 곱/소수의 곱.cc
@@ -1,35 +1,43 @@
 #include <bits/stdc++.h>
 #define FastIO cin.tie(0)->sync_with_stdio(0);
 using namespace std;
-using int64 = long long;
-const int MAX = 1 << 7;
+const long long INF = 2'147'483'647;
 
 int K, N;
+int P[101];
+int A[100001];
 
 int main() {
     FastIO
     cin >> K >> N;
-
-    vector<int64> P(K);
-    for (auto &x : P) cin >> x;
-
-    vector<int64> A;
-    A.reserve(N + 1);
-    A.push_back(1);
-
-    for (int i = 1; i <= N; i++) {
-        int64 cur = A.back();
-        int64 nxt = LLONG_MAX;
-
-        for (int64 x : P) {
-            int64 tmp = *upper_bound(A.begin(), A.end(), cur / x);
-            nxt = min(nxt, x * tmp);
-        }
-
-        A.push_back(nxt);
+    for (int i = 0; i < K; i++) {
+        cin >> P[i];
     }
 
-    int64 ans = A.back();
+    A[0] = 1;
+    for (int i = 1; i <= N; i++) {
+        int cur = A[i - 1];
+        int nxt = INF;
+
+        for (int k = 0; k < K; k++) {
+            int idx = -1;
+            int value = cur / P[k];
+            int low = 0, high = i - 1;
+
+            while (low <= high) {
+                int mid = (low + high) / 2;
+                if (A[mid] > value) idx = mid, high = mid - 1;
+                else low = mid + 1;
+            }
+            
+            long long tmp = 1LL * A[idx] * P[k];
+            if (tmp < INF) nxt = min(nxt, (int) tmp);
+        }
+
+        A[i] = nxt;
+    }
+
+    int ans = A[N];
     cout << ans << '\n';
     return 0;
 }
