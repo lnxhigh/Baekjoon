bbb1af1d524f55f0289841ec181dfe847e6296a4
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2025-01-30 10:29:33 +0900

    [Silver IV] Title: 대칭 차집합, Time: 72 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.cc
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.cc
@@ -1,17 +1,35 @@
 #include <bits/stdc++.h>
 #define FastIO cin.tie(0)->sync_with_stdio(0);
 using namespace std;
+const int MAX = 200001;
 
-int A, B, X; 
-unordered_set<int> S;
+int M, N; 
+int D[MAX];
+
+int find(int x) {
+    int low = 0, high = M;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (D[mid] < x) low = mid + 1;
+        else if (D[mid] > x) high = mid - 1;
+        else return mid;
+    }
+    return -1;
+}
 
 int main() {
     FastIO
-    cin >> A >> B;
-    while (A--) cin >> X, S.insert(X);
-
-    int cnt = S.size();
-    while (B--) cin >> X, cnt += S.count(X) ? -1 : +1;
+    cin >> M >> N;
+    for (int i = 0; i < M; i++) cin >> D[i];
+    sort(D, D + M);
+    
+    int cnt = M;
+    for (int i = 0; i < N; i++) {
+        int x; cin >> x;
+        int idx = find(x);
+        if (idx == -1) cnt++;
+        else cnt--;
+    }
 
     cout << cnt << '\n';
     return 0;
