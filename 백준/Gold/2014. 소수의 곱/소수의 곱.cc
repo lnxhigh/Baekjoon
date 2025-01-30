cf88502882f3333a9e6380b5aa4ae7ceacfd69a7
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2025-01-30 09:07:13 +0900

    [Gold I] Title: 소수의 곱, Time: 20 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/2014. 소수의 곱/소수의 곱.cc
+++ 백준/Gold/2014. 소수의 곱/소수의 곱.cc
@@ -6,38 +6,28 @@ const long long INF = 2'147'483'647;
 int K, N;
 int P[101];
 int A[100001];
+int D[101];
 
 int main() {
     FastIO
     cin >> K >> N;
-    for (int i = 0; i < K; i++) {
-        cin >> P[i];
-    }
+    for (int i = 0; i < K; i++) cin >> P[i];
 
     A[0] = 1;
     for (int i = 1; i <= N; i++) {
-        int cur = A[i - 1];
-        int nxt = INF;
-
+        long long nxt = INF;
         for (int k = 0; k < K; k++) {
-            int idx = -1;
-            int value = cur / P[k];
-            int low = 0, high = i - 1;
-
-            while (low <= high) {
-                int mid = (low + high) / 2;
-                if (A[mid] > value) idx = mid, high = mid - 1;
-                else low = mid + 1;
-            }
-            
-            long long tmp = 1LL * A[idx] * P[k];
-            if (tmp < INF) nxt = min(nxt, (int) tmp);
+            long long tmp = (long long) P[k] * A[D[k]];
+            nxt = min(nxt, tmp);
         }
 
         A[i] = nxt;
+        for (int k = 0; k < K; k++) {
+            long long tmp = (long long) P[k] * A[D[k]];
+            if (nxt == tmp) D[k]++;
+        }
     }
-
-    int ans = A[N];
-    cout << ans << '\n';
+    
+    cout << A[N] << '\n';
     return 0;
 }
