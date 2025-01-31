1c422eaa6f0b989b45c7e4ccec8c01c362515c2e
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2025-02-01 08:24:45 +0900

    [Gold III] Title: 사다리, Time: 0 ms, Memory: 2036 KB -BaekjoonHub

--- 백준/Gold/2528. 사다리/사다리.cc
+++ 백준/Gold/2528. 사다리/사다리.cc
@@ -4,57 +4,35 @@ using namespace std;
 const int MAX = 3000;
 
 int N, L;
-tuple<int,int,bool> A[MAX];
+int l[MAX];
+bool d[MAX];
 
-int f(int x, int len, bool dir, int t) {
-    if (len == L) return x;
+int f(int idx, int t) {
+    int len = l[idx];
+    bool dir = d[idx];
+    
+    if (len == L) return len / 2;
     t %= (L - len) * 2;
-    
-    int h = len / 2;
-    int l = x - h;
-    int r = L - (x + h);
-    
-    if (dir) {
-        if (t < l) return x - t;
-        else if (t < l + L - len) return (h) + (t - l);
-        else return (L - h) - (t - (l + L - len));
-    }
-    else {
-        if (t < r) return x + t;
-        else if (t < r + L - len) return (L - h) - (t - r);
-        else return (h) + (t - (r + L - len));
-    }
+
+    if (t >= L - len) t -= L - len, dir ^= true;
+    return dir ? (L - len / 2) - t : len / 2 + t;
 }
 
 int main() {
     FastIO
     cin >> N >> L;
     for (int i = 0; i < N; i++) {
-        int x, len;
-        bool dir;
-        cin >> len >> dir;
-        x = dir ? L - len / 2 : len / 2;
-        A[i] = { x, len, dir };
+        cin >> l[i] >> d[i];
     }
 
-    int t = 0;
-    for (int i = 1; i < N; i++) {
-        while (true) {
-            auto [x, xlen, xdir] = A[i - 1];
-            auto [y, ylen, ydir] = A[i];
-
-            int xnew = f(x, xlen, xdir, t);
-            int ynew = f(y, ylen, ydir, t);
-
-            bool canMove = false;
-            if (xnew <= ynew) canMove |= (xnew + xlen / 2) >= (ynew - ylen / 2);
-            else canMove |= (ynew + ylen / 2) >= (xnew - xlen / 2); 
-            
-            if (canMove) break;
-            else t++;
-        } 
+    int t = 0, i = 1;
+    while (i < N) {
+        int cur = f(i - 1, t), nxt = f(i, t);
+        int x = l[i - 1] / 2, y = l[i] / 2;
+        bool cross = (max(cur - x, nxt - y) <= min(cur + x, nxt + y));
+        cross ? i++ : t++;
     }
-
+    
     cout << t << '\n';
     return 0;
 }
