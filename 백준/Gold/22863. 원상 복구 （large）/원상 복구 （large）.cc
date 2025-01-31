9c0c92049d5b0ee19ea59e6594c03c8b34f7f3ab
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2025-01-31 19:31:47 +0900

    [Gold II] Title: 원상 복구 (large), Time: 316 ms, Memory: 19428 KB -BaekjoonHub

--- 백준/Gold/22863. 원상 복구 （large）/원상 복구 （large）.cc
+++ 백준/Gold/22863. 원상 복구 （large）/원상 복구 （large）.cc
@@ -1,58 +1,41 @@
 #include <bits/stdc++.h>
 #define FastIO cin.tie(0)->sync_with_stdio(0);
 using namespace std;
-using int64 = long long;
 const int MAX = 1 << 20;
 
 int N;
-int64 K;
-int S[MAX], D[MAX][20];
+long long K;
+int S[MAX], D[MAX];
 int P[MAX];
 
-int L;
-int len[MAX];
-int visited[MAX];
-
-int dfs(int x, int depth) {
-    if (visited[x] == 1) {
-        return len[x];
-    }
-    else if (visited[x] == 0) {
-        visited[x] = 1;
-        return len[x] = depth;
-    }
-    
-    visited[x] = 0;
-    len[x] = dfs(D[x][0], depth + 1);
-    visited[x] = 1;
-    return len[x];
-}
-
-int f(int x) {
-    int newK = K % dfs(x, 0);
-    for (int i = L - 1; i >= 0; i--) {
-        if (newK >> i & 1) x = D[x][i];
-    }
-    return x;
-}
+int A[MAX];
+bool V[MAX];
 
 int main() {
     FastIO
     cin >> N >> K;
     for (int i = 1; i <= N; i++) cin >> S[i];
-    for (int i = 1; i <= N; i++) cin >> D[i][0];
-    for (int i = N; i; i >>= 1) L++;
+    for (int i = 1; i <= N; i++) cin >> D[i];
 
-    for (int k = 1; k < L; k++) {
-        for (int i = 1; i <= N; i++) {
-            D[i][k] = D[D[i][k - 1]][k - 1];
+    for (int i = 1; i <= N; i++) {
+        if (P[i]) continue;
+
+        int cnt = 0;
+        for (int x = i; !V[x]; x = D[x]) {
+            V[x] = true;
+            A[cnt++] = x;
+        }
+
+        for (int x = 0; x < cnt; x++) { 
+            int cur = A[x];
+            int nxt = A[(K + x) % cnt];
+            P[nxt] = S[cur];
         }
     }
 
-    memset(visited, -1, sizeof(visited));
-    for (int i = 1; i <= N; i++) dfs(i, 0);
-
-    for (int i = 1; i <= N; i++) P[f(i)] = S[i];
-    for (int i = 1; i <= N; i++) cout << P[i] << ' ';
+    for (int i = 1; i <= N; i++) {
+        cout << P[i] << ' ';
+    }
+    cout << '\n';
     return 0;
 }
