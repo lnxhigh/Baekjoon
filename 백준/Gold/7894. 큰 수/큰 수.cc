191f6b56eb18e1deb1d090294641dd8a095616fd
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2025-02-28 10:00:52 +0900

    [Gold III] Title: 큰 수, Time: 0 ms, Memory: 2116 KB -BaekjoonHub

--- 백준/Gold/7894. 큰 수/큰 수.cc
+++ 백준/Gold/7894. 큰 수/큰 수.cc
@@ -1,22 +1,16 @@
 #include <bits/stdc++.h>
 #define FastIO cin.tie(0)->sync_with_stdio(0);
 using namespace std;
-const int MAX = 10'000'001;
-double D[MAX];
 
 int main() {
     FastIO
     int T; cin >> T;
 
-    for (int i = 1; i < MAX; i++) {
-        D[i] = D[i - 1] + log10(i);
-    }
-
     while (T--) {
         int N; cin >> N;
-        int K = D[N];
+        int K = (0.5 * log(2 * acos(-1) * N) + N * (log(N) - 1)) / log(10);
         cout << ++K << '\n';
     }
-    
+
     return 0;
 }
