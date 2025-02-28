abb9860950b4db649758537e2d19e9ebc6c79989
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2025-02-28 17:18:49 +0900

    [Gold III] Title: 울타리 치기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1364. 울타리 치기/울타리 치기.cc
+++ 백준/Gold/1364. 울타리 치기/울타리 치기.cc
@@ -1,25 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
-
-ll f(ll x) {
-    ll len = x / 6;
-    
-    ll r = x % 6;
-    if (r > 0) len++;
-
-    ll cnt = 3LL * len * (len + 1) + 1LL;
-    if (r == 0) return cnt;
-
-    len++;
-    r = 6 - r;
-
-    if (r--) cnt -= len;
-    while (r--) cnt -= len - 1;
-    return cnt;
-}
 
 int main() {
-    int N; cin >> N;
-    cout << f(N) << '\n';
+    long long N; cin >> N;
+    
+    // OEIS A001399
+    long long X = ((N + 3) * (N + 3) + 4) / 12;
+    cout << X << '\n';
+    return 0;
 }
