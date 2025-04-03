c7117f79189c275e7f0029d76061e3fbac3e5447
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2025-04-04 06:48:47 +0900

    [Gold II] Title: Wormholes, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9874. Wormholes/Wormholes.cc
+++ 백준/Gold/9874. Wormholes/Wormholes.cc
@@ -4,101 +4,54 @@ using namespace std;
 const int MAX = 1 << 4;
 
 int N;
-pair<int,int> Data[MAX];
-bool used[MAX];
+int X[MAX], Y[MAX];
+int walk[MAX], hole[MAX];
 
-vector<int> D;
-vector<pair<int,int>> matches;
-int ans = 0;
-
-int in(int x) { return x * 2; }
-int out(int x) { return x * 2 + 1; }
-
-bool check(vector<pair<int,int>>& match) {
-    vector<int> P = D;
-    for (auto& [x, y] : match) {
-        P[in(x)] = out(y), P[in(y)] = out(x);
+int f() {
+    for (int i = 1; i <= N; i++) {
+        int x = i;
+        for (int k = 1; k <= N && x; k++) {
+            x = walk[hole[x]];
+        }
+        if (x) return 1;
     }
-    
-    queue<int> q;
-    vector<int> deg(N * 2);
-
-    // Update degree
-    for (int i = 0; i < N * 2; i++) {
-        if (P[i] >= 0) deg[P[i]]++;
-    }
-
-    // Init queue
-    for (int i = 0; i < N * 2; i++) {
-        if (deg[i] == 0) q.push(i);
-    }
-
-    // Check cycle
-    for (int i = 0; i < N * 2; i++) {
-        if (q.empty()) return true;
-
-        int x = q.front();
-        q.pop();
-
-        int nxt = P[x];
-        if (nxt < 0) continue;
-        else if (--deg[nxt] == 0) q.push(nxt);
-    }
-
-    return false;
+    return 0;
 }
 
-void dfs(vector<pair<int,int>>& match) {
-    int idx = 0;
-    while (idx < N && used[idx]) idx++;
-    
-    if (idx >= N) {
-        bool stuck = check(match);
-        if (stuck) ans++;
-        return;
-    }
+int dfs() {
+    int ret = 0, x = 1;
+    while (x <= N && hole[x]) x++;
+    if (x > N) return f();
 
-    used[idx] = true;
-
-    for (int other = idx + 1; other < N; ++other) {
-        if (used[other]) continue;
-        
-        used[other] = true;
-        match.push_back({ idx, other });
-        dfs(match);
-        match.pop_back();
-        used[other] = false;
+    for (int y = x + 1; y <= N; y++) {
+        if (!hole[y]) {
+            hole[x] = y, hole[y] = x;
+            ret += dfs();
+            hole[x] = hole[y] = 0;
+        }
     }
     
-    used[idx] = false;
+    return ret;
 }
 
 int main() {
     FastIO
+
     cin >> N;
-    D = vector<int>(N * 2, -1);
-
-    for (int i = 0; i < N; i++) {
-        int x, y; cin >> x >> y;
-        Data[i] = { x, y };
+    for (int i = 1; i <= N; i++) {
+        cin >> X[i] >> Y[i];
     }
 
-    for (int i = 0; i < N; i++) {
-        int inIdx = -1;
-        auto [x, y] = Data[i];
-
-        for (int j = 0; j < N; j++) {
-            auto [nx, ny] = Data[j];
-            if (ny != y || nx <= x) continue;
-            if (inIdx == -1 || nx < Data[inIdx].first) inIdx = j;
+    for (int i = 1; i <= N; i++) {
+        int t = 0;
+        for (int j = 1; j <= N; j++) {
+            if (Y[j] != Y[i] || X[j] <= X[i]) continue;
+            if (t == 0 || X[j] < X[t]) t = j;
         }
-
-        // out -> in
-        if (inIdx != -1) D[out(i)] = in(inIdx);
+        walk[i] = t;
     }
 
-    dfs(matches);
-
+    int ans = dfs();
     cout << ans << '\n';
     return 0;
 }
