12d4f6211f20f0c9a1a62b23daaa3c98c22a26e0
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2025-04-26 04:03:42 +0900

    [Gold III] Title: HTML 파싱, Time: 12 ms, Memory: 3680 KB -BaekjoonHub

--- 백준/Gold/22859. HTML 파싱/HTML 파싱.cc
+++ 백준/Gold/22859. HTML 파싱/HTML 파싱.cc
@@ -4,24 +4,7 @@ using namespace std;
 
 int n;
 string document;
-
-vector<string> parse(const string &str) {
-    vector<string> ret;
-
-    int idx = 0;
-    int len = str.size();
-
-    while (idx < len) {
-        while (idx < len && str[idx] == ' ') idx++;
-
-        string word;
-        while (idx < len && str[idx] != ' ') word.push_back(str[idx++]);
-        
-        ret.push_back(word);
-    }
-
-    return ret;
-}
+bool space = false;
 
 int main() {
     FastIO
@@ -30,60 +13,52 @@ int main() {
     n = document.size();
 
     int ptr = 0;
-    string paragraph;
-
-    assert(document[0] != ' ');
 
     while (ptr < n) {
-        if (document[ptr] == '<') {
-            string content;
-            while (document[ptr] != '>') {
-                content.push_back(document[++ptr]);
+        string str;
+        bool flag = (document[ptr] == '<');
+        if (flag) ptr++;
+
+        while (ptr < n) {
+            char c = document[ptr];
+            if (c == '<') {
+                break;
             }
-            ++ptr;
-            content.pop_back();
+            else if (c == '>') {
+                ptr++;
+                break;
+            }
+            
+            str.push_back(c);
+            ptr++;
+        }
 
-            int x = 0;
-            while (x < (int) content.size() && content[x] == ' ') x++;
-
-            string tagname;
-            while (x < (int) content.size() && content[x] != ' ') tagname.push_back(content[x++]);
-            while (x < (int) content.size() && content[x] == ' ') x++;
-
-            if (tagname == "/p") {
-                vector<string> words = parse(paragraph);
-                int len = words.size();
-
-                for (int i = 0; i < len; i++) {
-                    cout << words[i];
-                    if (i != len - 1) cout << ' ';
+        if (flag) {
+            if (str.substr(0, 3) == "div") {
+                if (str.size() > 5u && str.substr(4, 5) == "title") {
+                    string title = str.substr(11);
+                    title.pop_back();
+                    cout << "title : " << title << '\n';
                 }
+            }
+            else if (str == "/p") {
                 cout << '\n';
-
-                paragraph.clear();
-            }
-            else if (tagname == "div") {
-                if (x < (int) content.size()) {
-                    // attribute 가 하나임을 가정함 
-                    // 안 되면 어떤 식으로 구현해야 할지 모르겠다
-                    string cur = content.substr(x);
-                    int idx = 0;
-                    while (idx < (int) cur.size() && cur[idx] != '=') idx++;
-                    
-                    // attribute 와 value 사이 띄어쓰기가 없음을 가정함
-                    string attribute = cur.substr(0, idx);
-                    if (attribute != "title") continue;
-
-                    int last = cur.size();
-                    while (last >= 0 && cur[last] != '"') last--;
-
-                    string value = cur.substr(idx + 2, last- idx - 2);
-                    cout << "title : " << value << '\n';
-                }
+                space = false;
             }
         }
         else {
-            paragraph.push_back(document[ptr++]);
+            for (char c : str) {
+                if (c == ' ') {
+                    space = true;
+                }
+                else {
+                    if (space) {
+                        cout << ' ';
+                        space = false;
+                    }
+                    cout << c;
+                }
+            }
         }
     }
 
