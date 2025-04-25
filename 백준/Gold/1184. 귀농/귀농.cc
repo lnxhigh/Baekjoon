bdfcfca1a70f158f2802345114611814ebdc787a
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2025-04-25 20:46:16 +0900

    [Gold I] Title: 귀농, Time: 12 ms, Memory: 41104 KB -BaekjoonHub

--- 백준/Gold/1184. 귀농/귀농.cc
+++ 백준/Gold/1184. 귀농/귀농.cc
@@ -2,10 +2,15 @@
 #define FastIO cin.tie(0)->sync_with_stdio(0);
 using namespace std;
 const int MAX = 51;
+const int OFFSET = 2500000;
 
 int n;
 int arr[MAX][MAX];
 int sum[MAX][MAX];
+
+int t = 1;
+int chk[5000001];
+int cnt[5000001];
 
 int area(int i, int j, int x, int y) {
     return sum[x][y] - sum[i - 1][y] - sum[x][j - 1] + sum[i - 1][j - 1];
@@ -27,39 +32,48 @@ int main() {
         }
     }
 
-    int cnt = 0;
+    int ans = 0;
 
     for (int i = 1; i < n; i++) {
         for (int j = 1; j < n; j++) {
-            unordered_map<int,int> first, second;
-
+            
             for (int x = 1; x <= i; x++) {
                 for (int y = 1; y <= j; y++) {
-                    first[area(x, y, i, j)]++;
+                    int a = area(x, y, i, j) + OFFSET;
+                    cnt[a] *= (chk[a] == t);
+                    chk[a] = t, cnt[a]++;
                 }
             }
 
             for (int x = i + 1; x <= n; x++) {
                 for (int y = j + 1; y <= n; y++) {
-                    cnt += first[area(i + 1, j + 1, x, y)];
+                    int a = area(i + 1, j + 1, x, y) + OFFSET;
+                    ans += cnt[a] * (chk[a] == t);
                 }
             }
 
+            t++;
+
             for (int x = i + 1; x <= n; x++) {
                 for (int y = 1; y <= j; y++) {
-                    second[area(i + 1, y, x, j)]++;
+                    int a = area(i + 1, y, x, j) + OFFSET;
+                    cnt[a] *= (chk[a] == t);
+                    chk[a] = t, cnt[a]++;
                 }
             }
 
             for (int x = 1; x <= i; x++) {
                 for (int y = j + 1; y <= n; y++) {
-                    cnt += second[area(x, j + 1, i, y)];
+                    int a = area(x, j + 1, i, y) + OFFSET;
+                    ans += cnt[a] * (chk[a] == t);
                 }
             }
+
+            t++;
         }
     }
     
-    cout << cnt << '\n';
+    cout << ans << '\n';
 
     return 0;
 }
