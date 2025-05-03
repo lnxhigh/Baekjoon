1067b238d70bf1d15c50225872326708ca9e43f5
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2025-05-04 07:57:09 +0900

    [Gold I] Title: 원뿔좌표계상의 거리, Time: 0 ms, Memory: 2120 KB -BaekjoonHub

--- 백준/Gold/1798. 원뿔좌표계상의 거리/원뿔좌표계상의 거리.cc
+++ 백준/Gold/1798. 원뿔좌표계상의 거리/원뿔좌표계상의 거리.cc
@@ -3,14 +3,12 @@
 using namespace std;
 const double PI = acos(-1);
 
-double r, p, d1, a1, d2, a2;
+double r, h, d1, a1, d2, a2;
 
 double solve() {
     double theta = abs(a2 - a1) / 180.0 * PI;
     theta = min(theta, 2.0 * PI - theta);
-    
-    double h = sqrt(r * r + p * p);
-    double phi = (r * theta) / h;
+    double phi = (r * theta) / sqrt(r * r + h * h);
     
     double len = sqrt(d1 * d1 + d2 * d2 - 2.0 * d1 * d2 * cos(phi));
     return len;
@@ -20,23 +18,8 @@ int main() {
     FastIO
     cout << fixed << setprecision(2);
 
-    string line;
-    while (getline(cin, line)) {
-        stringstream ss(line);
-        double arr[6] = {0.0};
-
-        for (int i = 0; i < 6; i++) {
-            string word;
-            getline(ss, word, ' ');
-            arr[i] = stod(word);
-        }
-        
-        r = arr[0], p = arr[1];
-        d1 = arr[2], a1 = arr[3];
-        d2 = arr[4], a2 = arr[5];
-
-        double ans = solve();
-        cout << ans << '\n';
+    while (cin >> r >> h >> d1 >> a1 >> d2 >> a2) {
+        cout << solve() << '\n';
     }
     
     return 0;
