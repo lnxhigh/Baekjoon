eb2ee8df347168c220809ce651743949f7eac1a8
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2025-05-05 05:38:06 +0900

    [Platinum IV] Title: 국회, Time: 24 ms, Memory: 2904 KB -BaekjoonHub

--- 백준/Platinum/1226. 국회/국회.cc
+++ 백준/Platinum/1226. 국회/국회.cc
@@ -1,59 +1,61 @@
 #include <bits/stdc++.h>
 #define FastIO cin.tie(0)->sync_with_stdio(0);
 using namespace std;
-const int MAX = 300;
+const int MAX = 100'001;
 
 int n;
 vector<pair<int,int>> arr;
 
 int sum = 0;
-bitset<100001> dp[MAX];
+bool dp[MAX];
+int now[MAX];
+int pre[MAX];
 
 int main() {
     FastIO
 
     cin >> n;
-    arr.resize(n);
     for (int i = 0; i < n; i++) {
         int x; cin >> x;
-        arr[i] = { x, i + 1 };
+        arr.emplace_back(x, i);
         sum += x;
     }
 
     sort(arr.rbegin(), arr.rend());
 
-    dp[0][0] = 1;
-    dp[0][arr[0].first] = 1;
+    dp[0] = true;
 
-    for (int i = 1; i < n; i++) {
-        dp[i] = dp[i - 1];
-        int x = arr[i].first;
-        int m = min(sum / 2, 100000 - x);
-        for (int j = 0; j <= m; j++) {
-            dp[i][j + x] = dp[i][j + x] | dp[i - 1][j];
+    for (int i = 0; i < n; i++) {
+        auto [x, idx] = arr[i];
+
+        for (int j = sum; j >= x; j--) {
+            if (!dp[j] && dp[j - x]) {
+                dp[j] = true;
+                now[j] = i;
+                pre[j] = j - x;
+            }
         }
     }
 
     int m = 0;
-    for (int i = 0; i <= 100000; i++) {
-        if (dp[n - 1][i]) m = i;
+    int half = sum / 2;
+
+    for (int x = half + 1; x <= sum; x++) {
+        if (!dp[x]) continue;
+
+        int v = arr[now[x]].first;
+        if (dp[x] && (x - v <= half)) m = x;
     }
 
     vector<int> items;
-    
-    for (int i = n - 1; i > 0; i--) {
-        auto [x, idx] = arr[i];
-        if ((!dp[i - 1][m]) && m >= x && dp[i - 1][m - x]) {
-            items.push_back(idx);
-            m -= x;
-        }
+
+    for (int x = m; x > 0; x = pre[x]) {
+        int idx = arr[now[x]].second;
+        items.emplace_back(idx);
     }
 
-    if (m > 0) items.push_back(arr[0].second);
-
-    sort(items.begin(), items.end());
     cout << items.size() << '\n';
-    for (int x : items) cout << x << ' ';
+    for (int idx : items) cout << idx + 1 << ' ';
     cout << '\n';
 
     return 0;
