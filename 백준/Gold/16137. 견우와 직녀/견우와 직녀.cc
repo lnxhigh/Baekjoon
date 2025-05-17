d85bd4128b8d677d32c6e848dba05099c314d6aa
Sanghwa Song <86676746+lnxhigh@users.noreply.github.com>
2025-05-18 05:24:42 +0900

    [Gold I] Title: 견우와 직녀, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/16137. 견우와 직녀/견우와 직녀.cc
+++ 백준/Gold/16137. 견우와 직녀/견우와 직녀.cc
@@ -5,71 +5,67 @@ using namespace std;
 int n, m;
 int arr[10][10];
 
-bool vis[10][10][2][5000];
+int dist[10][10][2];
 int dx[4] = { -1, 0, +1, 0 };
 int dy[4] = { 0, -1, 0, +1 };
-
-bool out(int x, int y) {
-    return (x < 0 || x >= n || y < 0 || y >= n);
-}
 
 bool check(int x, int y) {
     for (int i = 0; i < 4; i++) {
         int nx = x + dx[i], ny = y + dy[i];
         int rx = x + dx[(i + 1) % 4], ry = y + dy[(i + 1) % 4];
-        
-        if (out(nx, ny) || out(rx, ry)) continue;
+
+        if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+        if (rx < 0 || rx >= n || rx < 0 || rx >= n) continue;
         if (arr[nx][ny] != 1 && arr[rx][ry] != 1) return false;
     }
 
     return true;
 }
 
-int bfs() {
-    queue<tuple<int,int,int>> q;
-    q.push({ 0, 0, 0 });
-    vis[0][0][0][0] = true;
+int dijkstra() {
+    int ret = 1e9;
 
-    while (!q.empty()) {
-        auto [k, flag, t] = q.front();
-        q.pop();
+    for (int i = 0; i < n; i++) 
+        for (int j = 0; j < n; j++) 
+            for (int k = 0; k <= 1; k++) 
+                dist[i][j][k] = 1e9;
+    
+    priority_queue<tuple<int,int,bool>> pq;
+    pq.push({ 0, 0, false });
+    dist[0][0][0] = 0;
+
+    while (!pq.empty()) {
+        auto [t, k, f] = pq.top();
+        t = -t;
+        pq.pop();
 
         int x = k / n, y = k % n;
-        if (x == n - 1 && y == n - 1) return t;
-
-        if (t + 1 >= 5000) continue;
+        if (x == n - 1 && y == n - 1) ret = min(ret, t);
+        if (dist[x][y][f] < t) continue;
 
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i], ny = y + dy[i];
-            if (out(nx, ny)) continue;
+            if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+            else if (arr[x][y] != 1 && arr[nx][ny] != 1) continue;
+            else if (arr[nx][ny] == 0 && (f || !check(nx, ny))) continue;
 
-            if (arr[nx][ny] == 0) {
-                if (flag == 0 && (t + 1) % m == 0 && check(nx, ny) && arr[x][y] == 1) {
-                    if (!vis[nx][ny][1][t + 1]) {
-                        q.push({ nx * n + ny, 1, t + 1 });
-                        vis[nx][ny][1][t + 1] = true;
-                    }
-                }
-            }
-            else {
-                if ((t + 1) % arr[nx][ny] == 0 && (arr[x][y] == 1 || arr[nx][ny] == 1)) {
-                    if (!vis[nx][ny][flag][t + 1]) {
-                        q.push({ nx * n + ny, flag, t + 1 });
-                        vis[nx][ny][flag][t + 1] = true;
-                    }
-                }
-            }
-        }
-        
-        if (arr[x][y] == 1) {
-            if (!vis[x][y][flag][t + 1]) {
-                q.push({ x * n + y, flag, t + 1 });
-                vis[x][y][flag][t + 1] = true;
+            int p = 0;
+            bool add = false;
+
+            if (arr[nx][ny] == 0) p = m, add = true;
+            else p = arr[nx][ny], add = false;
+            
+            int nt = ((t + 1) % p) ? (t + 1) / p * p + p : (t + 1);
+            bool nf = f | add;
+            
+            if (nt < dist[nx][ny][nf]) {
+                dist[nx][ny][nf] = nt;
+                pq.push({ -nt, nx * n + ny, nf });
             }
         }
     }
 
-    return -1;
+    return ret;
 }
 
 int main() {
@@ -82,7 +78,7 @@ int main() {
         }
     }
 
-    int ans = bfs();
+    int ans = dijkstra();
     cout << ans << '\n';
 
     return 0;
