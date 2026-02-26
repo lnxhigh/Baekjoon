0d3bc55b0f40020584f117f9a29aac31986080b1
Sanghwa Song <cnrrn421@gmail.com>
2026-02-26 23:07:01 +0900

    [level 3] Title: 선입 선출 스케줄링, Time: 0.56 ms, Memory: 3.98 MB -BaekjoonHub

---
+++ 프로그래머스/3/12920. 선입 선출 스케줄링/선입 선출 스케줄링.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long f(int t, const vector<int> &cores) {
+    long long sum = cores.size();
+    for (int x : cores) sum += (t / x);
+    return sum;
+}
+
+int solution(int n, vector<int> cores) {
+    int low = 1;
+    int high = 10000 * 50000;
+    
+    int t = 0;
+    while (low <= high) {
+        int mid = (low + high) >> 1;
+        if (f(mid, cores) < n) t = mid, low = mid + 1;
+        else high = mid - 1;
+    }
+    
+    int cnt = n - f(t, cores);
+    int ans = 0;
+    
+    for (int i = 0; i < cores.size() && cnt > 0; i++) {
+        if ((t + 1) % cores[i] == 0) cnt--;
+        if (cnt == 0) ans = i;
+    }
+
+    return ++ans;
+}
