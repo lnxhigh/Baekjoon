333ce6eb2234e93e7f3b32c87a443ac0f7148cd0
Sanghwa Song <cnrrn421@gmail.com>
2026-04-25 11:33:17 +0900

    [Gold IV] Title: RGB거리 2, Time: 0 ms, Memory: 2068 KB -BaekjoonHub

--- 백준/Gold/17404. RGB거리 2/RGB거리 2.cc
+++ 백준/Gold/17404. RGB거리 2/RGB거리 2.cc
@@ -1,41 +1,57 @@
-#include <iostream>
+#include <bits/stdc++.h>
+#define FastIO cin.tie(0)->sync_with_stdio(0);
 using namespace std;
-int N;
-int cost[1000][3] = {0,};
-int MAX = 1000*1000;
-int ans = MAX;
-int Min(int a, int b) { return a < b ? a : b; }
-enum { R, G, B, K };
 
-int main(void) {
-    ios::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
+const int INF = 1 << 20;
+const int MAX = 1 << 10;
+array<int,3> A[MAX];
 
-    cin >> N;
-    for (int i = 0; i < N; i++) { 
-        cin >> cost[i][R] >> cost[i][G] >> cost[i][B]; 
+// D[i][color first][color last]
+// 1, 2, 3, ..., i번 집에 대해 비용의 최솟값을 구한다
+// 1번 집의 색과 i번 집의 색을 기록한다
+
+int D[MAX][3][3];
+enum Color { R, G, B };
+vector<Color> colors = { R, G, B };
+
+int main() {
+    FastIO
+    int N; cin >> N;
+    for (int i = 1; i <= N; i++) {
+        int r, g, b;
+        cin >> r >> g >> b;
+        A[i] = { r, g, b };
     }
 
-    for (int s = 0; s < K; s++) {
-        int house[1000][K];
+    // Initialize
+    fill(&D[0][0][0], &D[MAX-1][2][2] + 1, INF);
+    for (int col : colors) {
+        D[1][col][col] = A[1][col];
+    }
 
-        // 1. Fix the color of the first house
-        house[0][s] = cost[0][s];
-        house[0][(s+1)%K] = MAX; house[0][(s+2)%K] = MAX;
-
-        // 2. Fill the DP table
-        for (int i = 1; i < N; i++) {
-            for (int k = 0; k < K; k++) {
-                house[i][k] = Min(house[i-1][(k+1)%K], house[i-1][(k+2)%K]) + cost[i][k];
+    // Fill
+    for (int i = 2; i <= N; i++) {
+        for (int first : colors) {
+            for (int last : colors) {
+                for (int prev : colors) {
+                    if (last == prev) continue;
+                    D[i][first][last] = min(
+                        D[i][first][last], 
+                        D[i-1][first][prev] + A[i][last]
+                    );
+                }
             }
         }
-
-        // 3. Remove the case where the color of the first and the Nth house are the same
-        int value = Min(house[N-1][(s+1)%K], house[N-1][(s+2)%K]);
-        ans = Min(ans, value);
     }
 
-    cout << ans << '\n';
-    
+    // Result
+    int res = INF;
+    for (int first : colors) {
+        for (int last : colors) {
+            if (first != last) res = min(res, D[N][first][last]);
+        }
+    }
+    cout << res << '\n';
+
     return 0;
 }
